c4204fec4edf58b5c3be68dce3f97f9c200a84b3
YoonSuLee <ysleedev01@gmail.com>
2023-01-13 15:41:02 +0900

    Git 파일트리 수정

---
+++ SubwayWhen/DataLoad/ScheduleArrival/ScheduleSearch.swift
@@ -0,0 +1,16 @@
+//
+//  ScheduleSearch.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 2022/12/05.
+//
+
+import Foundation
+
+struct ScheduleSearch{
+    let stationCode : String
+    let upDown : String
+    let exceptionLastStation : String
+    let line : String
+    let type : ScheduleType
+}
