8b6eae4a0a97305b2997f4db89226043246b6ca0
YoonSuLee <ysleedev01@gmail.com>
2023-03-02 22:28:58 +0900

    Test: Network통신을 대체할 MockSession을 생성 후 NetworkManger의 디코딩 테스트 구문 추가

---
+++ SubwayWhenTests/NetworkMangerTest.swift
@@ -0,0 +1,56 @@
+//
+//  NetworkMangerTests.swift
+//  SubwayWhenTests
+//
+//  Created by 이윤수 on 2023/03/01.
+//
+
+import XCTest
+
+import RxSwift
+import RxTest
+import RxBlocking
+import RxOptional
+import Nimble
+
+@testable import SubwayWhen
+
+class NetworkMangerTests : XCTestCase{
+    let bag = DisposeBag()
+    
+    override func setUp() {
+    }
+    
+    func testNetworkManger(){
+        // GIVEN
+        let url = "Test.url"
+        guard let urlResponse = HTTPURLResponse(url: URL(string: url)!,
+                                          statusCode: 200,
+                                          httpVersion: nil,
+                                          headerFields: nil)
+        else {return}
+        
+        let data = Dummy().fileLoad("StationArrivalRequestDummy.json")
+        
+        let mockURL = MockURLSession((response: urlResponse, data: data))
+        let mockNetworkManger = NetworkManger(session: mockURL)
+        
+        // WHEN
+        let requestData = mockNetworkManger.requestData(url, dataType: LiveStationModel.self)
+        
+        let value = requestData
+            .map{ data -> LiveStationModel? in
+                guard case let .success(value) = data else {return nil}
+                return value
+            }
+            .map{$0!}
+            .asObservable()
+        
+        let blocking = value.toBlocking()
+        let arrayValue = try! blocking.toArray()
+        
+        // THEN (디코딩 테스트)
+        expect(arrayValue.first?.realtimeArrivalList.first?.stationName).to(equal("교대"))
+    
+    }
+}
