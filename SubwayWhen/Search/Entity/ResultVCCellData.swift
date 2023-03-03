3ccd639968b5277db0f93648e6c33f155cc327ed
YoonSuLee <ysleedev01@gmail.com>
2023-03-04 00:18:16 +0900

    Fix(SaveStation): 저장하는 StationName 중 부역명이 있는 경우 DetailVC에서 로드가 안되는 오류 수정

--- SubwayWhen/Search/Entity/ResultVCCellData.swift
+++ SubwayWhen/Search/Entity/ResultVCCellData.swift
@@ -15,6 +15,60 @@ struct ResultVCCellData{
     let stationCode : String
     let useLine : String
     let lineCode : String
+    
+    // 부역명 필수 지하철역
+    var useStationName : String{
+        switch self.stationName{
+        case "쌍용":
+            return "쌍용(나사렛대)"
+        case "총신대입구":
+            return "총신대입구(이수)"
+        case "신정":
+            return "신정(은행정)"
+        case "오목교":
+            return "오목교(목동운동장앞)"
+        case "군자":
+            return "군자(능동)"
+        case "아차산":
+            return "아차산(어린이대공원후문)"
+        case "광나루":
+            return "광나루(장신대)"
+        case "천호":
+            return "천호(풍납토성)"
+        case "굽은다리":
+            return "굽은다리(강동구민회관앞)"
+        case "새절":
+            return "새절(신사)"
+        case "증산":
+            return "증산(명지대앞)"
+        case "월드컵경기장":
+            return "월드컵경기장(성산)"
+        case "대흥":
+            return "대흥(서강대앞)"
+        case "안암":
+            return "안암(고대병원앞)"
+        case "월곡":
+            return "월곡(동덕여대)"
+        case "상월곡":
+            return "상월곡(한국과학기술연구원)"
+        case "화랑대":
+            return "화랑대(서울여대입구)"
+        case "공릉":
+            return "공릉(서울산업대입구)"
+        case "어린이대공원":
+            return "어린이대공원(세종대)"
+        case "이수":
+            return "총신대입구(이수)"
+        case "숭실대입구":
+            return "숭실대입구(살피재)"
+        case "몽촌토성":
+            return "몽촌토성(평화의문)"
+        case "남한산성입구":
+            return "남한산성입구(성남법원, 검찰청)"
+        default:
+            return self.stationName
+        }
+    }
 }
 
 extension ResultVCCellData : IdentifiableType, Equatable{
