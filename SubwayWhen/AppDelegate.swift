f89f1ee1958479d083c29c88a4ec31eb2c9611bf
YoonSuLee <ysleedev01@gmail.com>
2023-03-10 12:20:48 +0900

    Feat(App): LaunchScreen 구현

--- SubwayWhen/AppDelegate.swift
+++ SubwayWhen/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        // LanchScreen 지연
+        sleep(1)
+        
         return true
     }
 
