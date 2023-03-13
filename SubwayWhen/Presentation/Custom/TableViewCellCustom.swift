21d8862a99e503bad03cc2b1def6cdf42961323b
YoonSuLee <ysleedev01@gmail.com>
2023-03-13 12:02:05 +0900

    Fix(DetailVC): ExceptionBtn 클릭 시 Void Event를 다른 프로퍼티에 주는 오류 해결, info 위치 변경

--- SubwayWhen/Presentation/Custom/TableViewCellCustom.swift
+++ SubwayWhen/Presentation/Custom/TableViewCellCustom.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 import SnapKit
+import Then
 
 class TableViewCellCustom : UITableViewCell{
-    var mainBG = MainStyleUIView()
+    var mainBG = UIView().then{
+        $0.backgroundColor = UIColor(named: "MainColor")
+        $0.layer.cornerRadius = ViewStyle.Layer.radius
+        $0.layer.masksToBounds = true
+    }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
