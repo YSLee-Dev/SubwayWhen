2ba4e8246921c7b978d07fa917234567bafe2b5f
YoonSuLee <ysleedev01@gmail.com>
2023-03-15 18:59:15 +0900

    Refactor(DataLoad): TotalLoad 메소드를 korail, seoul로 분활

--- SubwayWhen/DataLoad/TotalLoadModel.swift
+++ SubwayWhen/DataLoad/TotalLoadModel.swift
@@ -58,158 +58,156 @@ class TotalLoadModel{
             .asObservable()
     }
     
-    // total 지하철 시간표
-    func totalScheduleStationLoad(_ scheduleSearch : ScheduleSearch, isFirst : Bool, isNow : Bool) -> Observable<[ResultSchdule]>{
+    // 코레일 시간표 계산
+    func korailSchduleLoad(scheduleSearch : ScheduleSearch, isFirst : Bool, isNow : Bool) ->  Observable<[ResultSchdule]>{
         guard let now = Int(self.timeFormatter(date: Date())) else {return .empty()}
-
-        if scheduleSearch.type == .Korail{
-            let requestRry = BehaviorSubject<Void>(value: Void())
-            var searchInfo = scheduleSearch
-            
-            let number = self.loadModel.korailTrainNumberLoad()
-            let request = requestRry
-                .throttle(.seconds(1), scheduler: MainScheduler.asyncInstance)
-                .flatMap{[weak self] _ in
-                    self?.loadModel.korailSchduleLoad(scheduleSearch: searchInfo) ?? .never()
-                }
-                .asObservable()
-            
-            let success = request
-                .map{data -> [KorailScdule] in
-                    guard case .success(let value) = data else {
-                        searchInfo.stationCode = searchInfo.stationCode.lowercased()
-                        requestRry.onNext(Void())
-                        requestRry.onCompleted()
-                        return []
-                    }
+        
+        let requestRry = BehaviorSubject<Void>(value: Void())
+        var searchInfo = scheduleSearch
+        
+        let number = self.loadModel.korailTrainNumberLoad()
+        let request = requestRry
+            .throttle(.seconds(1), scheduler: MainScheduler.asyncInstance)
+            .flatMap{[weak self] _ in
+                self?.loadModel.korailSchduleLoad(scheduleSearch: searchInfo) ?? .never()
+            }
+            .asObservable()
+        
+        let success = request
+            .map{data -> [KorailScdule] in
+                guard case .success(let value) = data else {
+                    searchInfo.stationCode = searchInfo.stationCode.lowercased()
+                    requestRry.onNext(Void())
                     requestRry.onCompleted()
-                    return value.body
+                    return []
                 }
-            
-            
-            // 상하행 구분 / 짝수일 경우 상행, 홀수일 경우 하행
-            let updownCheck = success.map{data in
-                return data.filter{
-                    let updownRequest = Int(String($0.trainCode.last ?? "9")) ?? 9
-                    if updownRequest % 2 == 0{
-                        return scheduleSearch.upDown == "상행" ? true : false
-                    }else{
-                        return scheduleSearch.upDown == "하행" ? true : false
-                    }
-                }
+                requestRry.onCompleted()
+                return value.body
             }
-            
-            let schedule = Observable<[ResultSchdule]>.combineLatest(number, updownCheck){number, updownCheck -> [ResultSchdule] in
-                var result : [ResultSchdule] = []
-                
-                // lastStation 값 주입
-                for r in updownCheck.enumerated(){
-                    result.append(ResultSchdule.init(startTime: r.element.time, type: .Korail, isFast: "", startStation: "", lastStation: ""))
-                    for n in number{
-                        if r.element.trainCode == n.trainNumber{
-                            result[r.offset].lastStation = n.endStation
-                            result[r.offset].startStation = n.startStation
-                            result[r.offset].isFast = n.isFast == "급행" ? "급행" : ""
-                            break
-                        }
-                    }
-                }
-                return result
-                    .sorted{
-                        let one = Int($0.startTime) ?? 0
-                        let two = Int($1.startTime) ?? 1
-                        
-                        return one<two
-                    }
-            }
-            
-            let filterData = schedule.map{ data in
-                data.filter{
-                    if isNow{
-                        if now <= Int($0.startTime) ?? 0 && !(scheduleSearch.exceptionLastStation.contains($0.lastStation)){
-                            return true
-                        }else{
-                            return false
-                        }
-                    }else{
-                        if !(scheduleSearch.exceptionLastStation.contains($0.lastStation)){
-                            return true
-                        }else{
-                            return false
-                        }
-                    }
-                }
-            }
-            
-           return filterData
-                .map{ list in
-                    if list.isEmpty{
-                        return [ResultSchdule(startTime: "정보없음", type: .Korail, isFast: "", startStation: "정보없음", lastStation: "정보없음")]
-                    }else{
-                        return list.map{
-                            ResultSchdule(startTime: $0.startTime, type: .Korail, isFast: $0.isFast, startStation: $0.startStation, lastStation: $0.lastStation)
-                        }
-                    }
-                }
-                
-        }else if scheduleSearch.type == .Seoul{
-            var inOut = ""
-            
-            // 9호선은 상하행이 반대
-            if scheduleSearch.line == "09호선"{
-                inOut = scheduleSearch.upDown.contains("상행") ? "2" : "1"
-            }else{
-                inOut = scheduleSearch.upDown.contains("상행") || scheduleSearch.upDown.contains("내선") ? "1" : "2"
-            }
-            
-            let schedule = self.loadModel.seoulStationScheduleLoad(scheduleSearch: scheduleSearch)
-                .map{ data -> [ScheduleStationArrival] in
-                    // success 되지 않으면 > 오류 발생 시
-                    guard case .success(let value) = data else {return []}
-                    return value.SearchSTNTimeTableByFRCodeService.row
-                }
-                .asObservable()
-            
-            print("SEOUL")
-            
-            return schedule.map{ data -> [ScheduleStationArrival] in
-                let scheduleData = data.filter{
-                    guard let scheduleTime = Int($0.startTime.components(separatedBy: ":").joined()) else {return false}
-                    
-                    if isNow{
-                        if scheduleSearch.stationCode == $0.stationCode && now <= scheduleTime && inOut == $0.upDown && !(scheduleSearch.exceptionLastStation.contains($0.lastStation)){
-                            return true
-                        }else{
-                            return false
-                        }
-                    }else{
-                        if scheduleSearch.stationCode == $0.stationCode && inOut == $0.upDown && !(scheduleSearch.exceptionLastStation.contains($0.lastStation)){
-                            return true
-                        }else{
-                            return false
-                        }
-                    }
-                }
-                
-                if isFirst{
-                    guard let first = scheduleData.first else {return []}
-                    return [first]
+        
+        // 상하행 구분 / 짝수일 경우 상행, 홀수일 경우 하행
+        let updownCheck = success.map{data in
+            return data.filter{
+                let updownRequest = Int(String($0.trainCode.last ?? "9")) ?? 9
+                if updownRequest % 2 == 0{
+                    return scheduleSearch.upDown == "상행" ? true : false
                 }else{
-                    return scheduleData
+                    return scheduleSearch.upDown == "하행" ? true : false
                 }
             }
+        }
+        
+        let schedule = Observable<[ResultSchdule]>.combineLatest(number, updownCheck){number, updownCheck -> [ResultSchdule] in
+            var result : [ResultSchdule] = []
+            
+            // lastStation 값 주입
+            for r in updownCheck.enumerated(){
+                result.append(ResultSchdule.init(startTime: r.element.time, type: .Korail, isFast: "", startStation: "", lastStation: ""))
+                for n in number{
+                    if r.element.trainCode == n.trainNumber{
+                        result[r.offset].lastStation = n.endStation
+                        result[r.offset].startStation = n.startStation
+                        result[r.offset].isFast = n.isFast == "급행" ? "급행" : ""
+                        break
+                    }
+                }
+            }
+            
+            return result
+                .sorted{
+                    let one = Int($0.startTime) ?? 0
+                    let two = Int($1.startTime) ?? 1
+                    
+                    return one<two
+                }
+        }
+        
+        let filterData = schedule.map{ data in
+            data.filter{
+                if isNow{
+                    if now <= Int($0.startTime) ?? 0 && !(scheduleSearch.exceptionLastStation.contains($0.lastStation)){
+                        return true
+                    }else{
+                        return false
+                    }
+                }else{
+                    if !(scheduleSearch.exceptionLastStation.contains($0.lastStation)){
+                        return true
+                    }else{
+                        return false
+                    }
+                }
+            }
+        }
+        
+        return filterData
             .map{ list in
                 if list.isEmpty{
-                    return [ResultSchdule(startTime: "정보없음", type: .Seoul, isFast: "", startStation: "정보없음", lastStation: "정보없음")]
+                    return [ResultSchdule(startTime: "정보없음", type: .Korail, isFast: "", startStation: "정보없음", lastStation: "정보없음")]
                 }else{
                     return list.map{
-                        ResultSchdule(startTime: $0.startTime, type: .Seoul, isFast: $0.isFast == "D" ? "급행" : "", startStation: $0.startStation, lastStation: $0.lastStation)
+                        ResultSchdule(startTime: $0.startTime, type: .Korail, isFast: $0.isFast, startStation: $0.startStation, lastStation: $0.lastStation)
                     }
                 }
             }
+    }
+    
+    // 서울 시간표 계산
+    func seoulScheduleLoad(_ scheduleSearch : ScheduleSearch, isFirst : Bool, isNow : Bool) -> Observable<[ResultSchdule]>{
+        guard let now = Int(self.timeFormatter(date: Date())) else {return .empty()}
+        
+        var inOut = ""
+        
+        // 9호선은 상하행이 반대
+        if scheduleSearch.line == "09호선"{
+            inOut = scheduleSearch.upDown.contains("상행") ? "2" : "1"
         }else{
-            return Observable<[ResultSchdule]>
-                .just([ResultSchdule(startTime: "정보없음", type: .Unowned, isFast: "", startStation: "정보없음", lastStation: "정보없음")])
+            inOut = scheduleSearch.upDown.contains("상행") || scheduleSearch.upDown.contains("내선") ? "1" : "2"
+        }
+        
+        let schedule = self.loadModel.seoulStationScheduleLoad(scheduleSearch: scheduleSearch)
+            .map{ data -> [ScheduleStationArrival] in
+                // success 되지 않으면 > 오류 발생 시
+                guard case .success(let value) = data else {return []}
+                return value.SearchSTNTimeTableByFRCodeService.row
+            }
+            .asObservable()
+        
+        
+        return schedule.map{ data -> [ScheduleStationArrival] in
+            let scheduleData = data.filter{
+                guard let scheduleTime = Int($0.startTime.components(separatedBy: ":").joined()) else {return false}
+                
+                if isNow{
+                    if scheduleSearch.stationCode == $0.stationCode && now <= scheduleTime && inOut == $0.upDown && !(scheduleSearch.exceptionLastStation.contains($0.lastStation)){
+                        return true
+                    }else{
+                        return false
+                    }
+                }else{
+                    if scheduleSearch.stationCode == $0.stationCode && inOut == $0.upDown && !(scheduleSearch.exceptionLastStation.contains($0.lastStation)){
+                        return true
+                    }else{
+                        return false
+                    }
+                }
+            }
+            
+            if isFirst{
+                guard let first = scheduleData.first else {return []}
+                return [first]
+            }else{
+                return scheduleData
+            }
+        }
+        .map{ list in
+            if list.isEmpty{
+                return [ResultSchdule(startTime: "정보없음", type: .Seoul, isFast: "", startStation: "정보없음", lastStation: "정보없음")]
+            }else{
+                return list.map{
+                    ResultSchdule(startTime: $0.startTime, type: .Seoul, isFast: $0.isFast == "D" ? "급행" : "", startStation: $0.startStation, lastStation: $0.lastStation)
+                }
+            }
         }
     }
     
