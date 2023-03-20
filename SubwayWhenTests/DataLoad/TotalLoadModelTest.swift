6f78a2efbe45f3c52aaa4c5a5c41dd8e0eb0c610
YoonSuLee <ysleedev01@gmail.com>
2023-03-20 16:41:53 +0900

    Test(LoadModel): 지하철 data가 error 상황일 때(없을 때) test 하는 구문 추가

--- SubwayWhenTests/DataLoad/TotalLoadModelTest.swift
+++ SubwayWhenTests/DataLoad/TotalLoadModelTest.swift
@@ -16,6 +16,7 @@ import RxBlocking
 
 final class TotalLoadModelTest: XCTestCase {
     var arrivalTotalLoadModel : TotalLoadModel!
+    var arrivalErrorTotalLoadModel : TotalLoadModel!
     
     
     override func setUp(){
@@ -24,6 +25,8 @@ final class TotalLoadModelTest: XCTestCase {
         let loadModel = LoadModel(networkManager: networkManager)
         
         self.arrivalTotalLoadModel = TotalLoadModel(loadModel: loadModel)
+        
+        self.arrivalErrorTotalLoadModel = TotalLoadModel(loadModel: LoadModel(networkManager: NetworkManager(session: MockURLSession(((response: urlResponse!, data: arrivalErrorData))))))
     }
     
     func testTotalLiveDataLoad(){
@@ -69,4 +72,39 @@ final class TotalLoadModelTest: XCTestCase {
             description: "기본 데이터가 같으므로, code 또한 동일해야함"
         )
     }
+    
+    func testTotalLiveDataLoadError(){
+        //GIVEN
+        let data = self.arrivalErrorTotalLoadModel.totalLiveDataLoad(stations: [SaveStation(id: "-", stationName: "교대", stationCode: "330", updnLine: "상행", line: "03호선", lineCode: "1003", group: .one, exceptionLastStation: "", korailCode: "")])
+        
+        let blocking = data.toBlocking()
+        let arrayData = try! blocking.toArray()
+        
+        // WHEN
+        let requestCode = arrayData.first?.code
+        let dummyStationCode = "현재 실시간 열차 데이터가 없어요."
+        
+        let requestStationName = arrayData.first?.stationName
+        let dummyStationName = "교대"
+        
+        let requestBackId = arrayData.first?.backStationId
+        let dummyBackId = ""
+        
+        // THEN
+        expect(requestCode).to(
+        equal(dummyStationCode),
+        description: "열차 데이터를 받아오지 못할 때는 (현재 실시간 열차 데이터가 없어요.)가 나와야함"
+        )
+        
+        expect(requestStationName).to(
+        equal(dummyStationName),
+        description: "열차 데이터를 받아오지 못해도 역 이름은 동일해야함"
+        )
+        
+        expect(requestBackId).to(
+        equal(dummyBackId),
+        description: "열차 데이터를 받아오지 못하면 Back, NextId가 없어야 함"
+        )
+    }
+    
 }
