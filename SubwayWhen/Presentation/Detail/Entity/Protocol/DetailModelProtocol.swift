337bb4336c1d49028a2497b95de05096956a0385
YoonSuLee <ysleedev01@gmail.com>
2023-03-23 16:58:37 +0900

    Refactor(DetailVC): DetailViewModel를 메소드 단위로 분리하여 DetailModel에서 통신 및 데이터를 작업하도록 변경

--- SubwayWhen/Presentation/Detail/Entity/Protocol/DetailModelProtocol.swift
+++ SubwayWhen/Presentation/Detail/Entity/Protocol/DetailModelProtocol.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+import RxSwift
+
 protocol DetailModelProtocol{
     func nextAndBackStationSearch(backId : String, nextId : String) -> [String]
+    func mainCellDataToScheduleSearch(_ item : MainTableViewCellData) -> ScheduleSearch
+    func mainCellDataToDetailSection(_ data : MainTableViewCellData) -> [DetailTableViewSectionData]
+    func scheduleLoad(_ data : ScheduleSearch) -> Observable<[ResultSchdule]>
+    func arrvialDataLoad(_ station : String) -> Observable<[RealtimeStationArrival]>
+    func arrivalDataMatching(station : MainTableViewCellData, arrivalData : [RealtimeStationArrival]) -> [RealtimeStationArrival]
 }
