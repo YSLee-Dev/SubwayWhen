7442fca7934a5f8a66fbe5fa0e9b11dab432805b
YoonSuLee <ysleedev01@gmail.com>
2023-03-27 20:16:57 +0900

    Test(ReportVC): ReportViewModel, ReportModel 테스트 코드 추가

---
+++ SubwayWhenTests/Report/ReportModelTests.swift
@@ -0,0 +1,291 @@
+//
+//  ReportModelTests.swift
+//  SubwayWhenTests
+//
+//  Created by 이윤수 on 2023/03/27.
+//
+
+import XCTest
+
+import RxSwift
+import RxCocoa
+import RxBlocking
+import Nimble
+
+@testable import SubwayWhen
+
+final class ReportModelTests: XCTestCase {
+    var model : ReportModelProtocol!
+    
+    override func setUp() {
+        self.model = ReportModel()
+    }
+    
+    func testLineListData(){
+        // GIVEN
+        let data = model.lineListData()
+        let blocking = data.toBlocking()
+        let arrayData = try! blocking.toArray()
+        
+        // WHEN
+        let requestCount = arrayData.first?.count
+        let dummyCount = 15
+        
+        let requestLastLine = arrayData.first?.last
+        let dummyLastLine = "신분당선"
+        
+        // THEN
+        expect(requestCount).to(
+            equal(dummyCount),
+            description: "count는 14개로 고정되어 있어야함"
+        )
+        
+        expect(requestLastLine).to(
+            equal(dummyLastLine),
+            description: "모든 데이터는 동일해야함"
+        )
+    }
+    
+    func testOneQuestionData(){
+        // GIVEN
+        let data = self.model.oneStepQuestionData()
+        let blocking = data.toBlocking()
+        let arrayData = try! blocking.toArray()
+        
+        // WHEN
+        let requestItemCount = arrayData.first?.first?.items.count
+        let dummyItemCount = 1
+        
+        let requestSectionName = arrayData.first?.first?.sectionName
+        let dummySectionName = "민원 호선"
+        
+        let requestItemType = arrayData.first?.first?.items.first?.type
+        let dummyItemType = ReportTableViewCellType.Line
+        
+        // THEN
+        expect(requestItemCount).to(
+            equal(dummyItemCount),
+            description: "count는 1개로 고정되어 있어야함"
+        )
+        
+        expect(requestSectionName).to(
+            equal(dummySectionName),
+            description: "모든 데이터는 동일해야함"
+        )
+        
+        expect(requestItemType).to(
+            equal(dummyItemType),
+            description: "타입은 동일해야함"
+        )
+    }
+    
+    func testTwoStepQuestionData(){
+        // GIVEN
+        let data = self.model.twoStepQuestionData()
+        
+        // WHEN
+        let requestItemCount = data.items.count
+        let dummyItemCount = 2
+        
+        let requestSectionName = data.sectionName
+        let dummySectionName = "호선 정보"
+        
+        let requestFirstFocus = data.items.first?.focus
+        let dummyFirstFocus = true
+        
+        // THEN
+        expect(requestItemCount).to(
+            equal(dummyItemCount),
+            description: "count는 2개로 고정되어 있어야함"
+        )
+        
+        expect(requestSectionName).to(
+            equal(dummySectionName),
+            description: "모든 데이터는 동일해야함"
+        )
+        
+        expect(requestFirstFocus).to(
+            equal(dummyFirstFocus),
+            description: "첫 번째 값의 포커스는 True여야함"
+        )
+    }
+    
+    func testStepSideException(){
+        // GIVEN
+        let reportData = ReportBrandData.three
+        let data = self.model.twoStepSideException(reportData)
+        
+        // WHEN
+        let requestType = data?.type
+        let dummyType = ReportTableViewCellType.TwoBtn
+        
+        let requestCellTitle = data?.cellTitle
+        let dummyCellTitle = "현재 역이 지축 ~ 대화 안에 있나요?"
+        
+        // THEN
+        expect(requestType).to(
+            equal(dummyType),
+            description: "타입은 TwoBtn으로 고정되어 있어야함"
+        )
+        
+        expect(requestCellTitle).to(
+            equal(dummyCellTitle),
+            description: "3호선으로 두었을 때는 지축 ~ 대화 메세지가 나와야함"
+        )
+    }
+    
+    func testStepSideExceptionError(){
+        // GIVEN
+        let reportData = ReportBrandData.airport
+        let data = self.model.twoStepSideException(reportData)
+        
+        // WHEN
+        let requestType = data?.type
+        
+        let requestCellTitle = data?.cellTitle
+        
+        // THEN
+        expect(requestType).to(
+            beNil(),
+            description: "1,3,4호선이 아니면 nil이 나와야함"
+        )
+        
+        expect(requestCellTitle).to(
+            beNil(),
+            description: "1,3,4호선이 아니면 nil이 나와야함"
+        )
+    }
+    
+    func testThreeStepQuestion(){
+        // GIVEN
+        let data = self.model.theeStepQuestion()
+        
+        // WHEN
+        let requestItemCount = data.items.count
+        let dummyItemCount = 1
+        
+        let requestSectionName = data.sectionName
+        let dummySectionName = "상세 정보"
+        
+        let requestFirstFocus = data.items.first?.focus
+        let dummyFirstFocus = true
+        
+        // THEN
+        expect(requestItemCount).to(
+            equal(dummyItemCount),
+            description: "count는 1개로 고정되어 있어야함"
+        )
+        
+        expect(requestSectionName).to(
+            equal(dummySectionName),
+            description: "모든 데이터는 동일해야함"
+        )
+        
+        expect(requestFirstFocus).to(
+            equal(dummyFirstFocus),
+            description: "첫 번째 값의 포커스는 True여야함"
+        )
+    }
+    
+    func testCellDataMatching(){
+        // GIVEN
+        let index = IndexPath(row: 1, section: 1)
+        let matchIndex = IndexPath(row: 1, section: 1)
+        let info = "INFO"
+        
+        let data = self.model.cellDataMatching(index: index, matchIndex: matchIndex, data: info)
+        
+        // WHEN
+        let requestData = data
+        let dummyData = info
+        
+        // THEN
+        expect(requestData).to(
+            equal(dummyData),
+            description: "Index가 같으므로, 값이 나와야함"
+        )
+    }
+    
+    func testCellDataMatchingError(){
+        // GIVEN
+        let index = IndexPath(row: 0, section: 1)
+        let matchIndex = IndexPath(row: 1, section: 1)
+        let info = "INFO"
+        
+        let data = self.model.cellDataMatching(index: index, matchIndex: matchIndex, data: info)
+        
+        // WHEN
+        let requestData = data
+        
+        // THEN
+        expect(requestData).to(
+            beNil(),
+            description: "Index가 다르면, nil이 나와야함"
+        )
+    }
+    
+    func testCellDataSave(){
+        let nowData : [ReportTableViewCellSection] = [
+            .init(sectionName: "정보", items: [
+                .init(cellTitle: "타이틀", cellData: "셀정보", type: .TextField, focus: true),
+                .init(cellTitle: "타이틀2", cellData: "셀정보2", type: .TextField, focus: false)
+            ]),
+            .init(sectionName: "정보2", items: [
+                .init(cellTitle: "타이틀3", cellData: "타이틀3", type: .TwoBtn, focus: true)
+            ])
+        ]
+        
+        let index = IndexPath(row: 0, section: 1)
+        let info = "SAVEDATA"
+        let data = self.model.cellDataSave(nowData: nowData, data: info, index: index)
+        
+        // WHEN
+        let requestCellData = data[index.section].items[index.row].cellData
+        let dummyCellData = info
+        
+        let requestCellFocus = data[index.section].items[index.row].focus
+        let dummyCellFocus = false
+        
+        let requestCellTitle = data[index.section].items[index.row].cellTitle
+        let dummyCellTitle = "타이틀3"
+        
+        // THEN
+        expect(requestCellData).to(
+            equal(dummyCellData),
+            description: "indexPath에 맞게 데이터가 변경되어야함"
+        )
+        
+        expect(requestCellFocus).to(
+            equal(dummyCellFocus),
+            description: "포커스는 false가 되어야함"
+        )
+        
+        expect(requestCellTitle).to(
+            equal(dummyCellTitle),
+            description: "포커스, 데이터 이외의 값은 변경되면 안됨"
+        )
+    }
+    
+    func testCellDataSaveError(){
+        let nowData : [ReportTableViewCellSection] = [
+            .init(sectionName: "정보", items: [
+                .init(cellTitle: "타이틀", cellData: "셀정보", type: .TextField, focus: true),
+                .init(cellTitle: "타이틀2", cellData: "셀정보2", type: .TextField, focus: false)
+            ]),
+            .init(sectionName: "정보2", items: [
+                .init(cellTitle: "타이틀3", cellData: "타이틀3", type: .TwoBtn, focus: true)
+            ])
+        ]
+        
+        let index = IndexPath(row: 2, section: 1)
+        let info = "SAVEDATA"
+        let data = self.model.cellDataSave(nowData: nowData, data: info, index: index)
+        
+        // WHEN
+        expect(data).toNot(
+            beNil(),
+            description: "Index값을 넘어도 앱이 크레쉬 나지 않아야함"
+        )
+    }
+    
+}
---
+++ SubwayWhenTests/Report/ReportViewModelTests.swift
@@ -0,0 +1,408 @@
+//
+//  ReportViewModelTests.swift
+//  SubwayWhenTests
+//
+//  Created by 이윤수 on 2023/03/27.
+//
+
+import XCTest
+
+import RxSwift
+import RxCocoa
+import RxTest
+import Nimble
+
+@testable import SubwayWhen
+
+final class ReportViewModelTests: XCTestCase {
+    var model : ReportModelProtocol!
+    let bag = DisposeBag()
+    
+    override func setUp(){
+        self.model = ReportModel()
+    }
+    
+    func testOne_TwoStep(){
+        // GIVEN
+        let scheduler = TestScheduler(initialClock: 0)
+        let nowData = BehaviorRelay<[ReportTableViewCellSection]>(value: [])
+        let observer = scheduler.createObserver([ReportTableViewCellSection].self)
+        
+        nowData
+            .subscribe(observer)
+            .disposed(by: self.bag)
+        
+        let oneStep = self.model.oneStepQuestionData()
+        let blocking = oneStep.toBlocking()
+        let oneStepData = try! blocking.toArray().first
+        
+        // WHEN
+        let oneStepObservable = scheduler.createHotObservable([
+            .next(1, oneStepData!)
+        ])
+        
+        oneStepObservable
+            .bind(to: nowData)
+            .disposed(by: self.bag)
+        
+        // TAP
+        let tapData = PublishSubject<ReportBrandData>()
+        let tap = PublishSubject<Void>()
+        
+        let tapEvnet = scheduler.createColdObservable([
+            .next(3, ReportBrandData.one)
+        ])
+            .share()
+        
+        tapEvnet
+            .subscribe(tapData)
+            .disposed(by: self.bag)
+        
+        tapEvnet
+            .map{_ in Void()}
+            .subscribe(tap)
+            .disposed(by: self.bag)
+        
+        tap
+            .withLatestFrom(tapData)
+            .withUnretained(self)
+            .map{ cell, data in
+                let value = nowData.value
+                let first = value.first!
+                
+                var two = cell.model.twoStepQuestionData()
+                
+                if let exception = cell.model.twoStepSideException(data) {
+                    two.items.append(exception)
+                }
+                
+                return [first, two]
+            }
+            .debug()
+            .bind(to: nowData)
+            .disposed(by: self.bag)
+        
+        scheduler.start()
+        
+        // THEN
+        expect(observer.events).to(
+            equal([
+                .next(0, []),
+                .next(1, [ReportTableViewCellSection(sectionName: "민원 호선", items: [.init(cellTitle: "몇호선 민원을 접수하시겠어요?", cellData: "", type: .Line, focus: false)])]),
+                .next(3, [
+                    ReportTableViewCellSection(sectionName: "민원 호선", items: [.init(cellTitle: "몇호선 민원을 접수하시겠어요?", cellData: "", type: .Line, focus: false)]),
+                    ReportTableViewCellSection(sectionName: "호선 정보", items: [
+                        .init(cellTitle: "열차의 행선지를 입력해주세요.", cellData: "", type: .TextField, focus: true),
+                        .init(cellTitle: "현재 역을 입력해주세요.", cellData: "", type: .TextField, focus: false),
+                        .init(cellTitle: "현재 역이 청량리 ~ 서울역 안에 있나요?", cellData: "", type: .TwoBtn, focus: false)
+                    ])
+                ])
+            ])
+        )
+    }
+    
+    func testTwo_ThreeStep(){
+        // GIVEN
+        let scheduler = TestScheduler(initialClock: 0)
+        let nowData = BehaviorRelay<[ReportTableViewCellSection]>(value: [])
+        let observer = scheduler.createObserver([ReportTableViewCellSection].self)
+        
+        nowData
+            .subscribe(observer)
+            .disposed(by: self.bag)
+        
+        let step = scheduler.createHotObservable([
+            .next(1, [ReportTableViewCellSection(sectionName: "민원 호선", items: [.init(cellTitle: "몇호선 민원을 접수하시겠어요?", cellData: "", type: .Line, focus: false)])]),
+            .next(3, [
+                ReportTableViewCellSection(sectionName: "민원 호선", items: [.init(cellTitle: "몇호선 민원을 접수하시겠어요?", cellData: "", type: .Line, focus: false)]),
+                ReportTableViewCellSection(sectionName: "호선 정보", items: [
+                    .init(cellTitle: "열차의 행선지를 입력해주세요.", cellData: "", type: .TextField, focus: true),
+                    .init(cellTitle: "현재 역을 입력해주세요.", cellData: "", type: .TextField, focus: false),
+                    .init(cellTitle: "현재 역이 청량리 ~ 서울역 안에 있나요?", cellData: "", type: .TwoBtn, focus: false)
+                ])
+            ])
+        ])
+        
+        step
+            .bind(to: nowData)
+            .disposed(by: self.bag)
+        
+        // WHEN
+        // 설명
+        let destination = PublishSubject<String>()
+        let destinationEvnet = scheduler.createHotObservable([
+            .next(5, "인천행")
+        ])
+        destinationEvnet
+            .subscribe(destination)
+            .disposed(by: self.bag)
+        
+        // 역
+        let nowStation = PublishSubject<String>()
+        let nowStationEvent = scheduler.createHotObservable([
+            .next(8, "시청역")
+        ])
+        nowStationEvent
+            .subscribe(nowStation)
+            .disposed(by: self.bag)
+        
+        // 선택
+        let brand = PublishSubject<String>()
+        let brandEvnet = scheduler.createHotObservable([
+            .next(7, "Y")
+        ])
+        brandEvnet
+            .subscribe(brand)
+            .disposed(by: self.bag)
+        
+        destination
+            .map{[weak self] data in
+                var now = nowData.value
+                now = self!.model.cellDataSave(nowData: now , data: data, index: IndexPath(row: 0, section: 1))
+                
+                if now[1].items[1].cellData == ""{
+                    now[1].items[1].focus = true
+                }
+                
+                return now
+            }
+            .bind(to: nowData)
+            .disposed(by: self.bag)
+        
+        nowStation
+            .map{[weak self] data in
+                var now = nowData.value
+                now = self!.model.cellDataSave(nowData: now , data: data, index: IndexPath(row: 1, section: 1))
+                
+                return now
+            }
+            .bind(to: nowData)
+            .disposed(by: self.bag)
+        
+        Observable.combineLatest(destination, nowStation, brand)
+            .withUnretained(self)
+            .map{cell, value -> [ReportTableViewCellSection] in
+                var now = nowData.value
+                
+                if now[1].items.count == 3 && value.2 == "N/A"{
+                    return []
+                }else if value.2 != "N/A"{
+                    now[1].items[2].cellData = value.2
+                }
+                
+                now.append(cell.model.theeStepQuestion())
+                
+                return now
+                
+            }
+            .bind(to: nowData)
+            .disposed(by: self.bag)
+        
+        scheduler.start()
+        
+        // THEN
+        expect(observer.events).to(
+            equal([
+                .next(0, []),
+                .next(1, [ReportTableViewCellSection(sectionName: "민원 호선", items: [.init(cellTitle: "몇호선 민원을 접수하시겠어요?", cellData: "", type: .Line, focus: false)])]),
+                .next(3, [
+                    ReportTableViewCellSection(sectionName: "민원 호선", items: [.init(cellTitle: "몇호선 민원을 접수하시겠어요?", cellData: "", type: .Line, focus: false)]),
+                    ReportTableViewCellSection(sectionName: "호선 정보", items: [
+                        .init(cellTitle: "열차의 행선지를 입력해주세요.", cellData: "", type: .TextField, focus: true),
+                        .init(cellTitle: "현재 역을 입력해주세요.", cellData: "", type: .TextField, focus: false),
+                        .init(cellTitle: "현재 역이 청량리 ~ 서울역 안에 있나요?", cellData: "", type: .TwoBtn, focus: false)
+                    ])
+                ]),
+                .next(5,[
+                    ReportTableViewCellSection(sectionName: "민원 호선", items: [.init(cellTitle: "몇호선 민원을 접수하시겠어요?", cellData: "", type: .Line, focus: false)]),
+                    ReportTableViewCellSection(sectionName: "호선 정보", items: [
+                        .init(cellTitle: "열차의 행선지를 입력해주세요.", cellData: "인천행", type: .TextField, focus: false),
+                        .init(cellTitle: "현재 역을 입력해주세요.", cellData: "", type: .TextField, focus: true),
+                        .init(cellTitle: "현재 역이 청량리 ~ 서울역 안에 있나요?", cellData: "", type: .TwoBtn, focus: false)
+                    ])
+                ]),
+                .next(8,[
+                    ReportTableViewCellSection(sectionName: "민원 호선", items: [.init(cellTitle: "몇호선 민원을 접수하시겠어요?", cellData: "", type: .Line, focus: false)]),
+                    ReportTableViewCellSection(sectionName: "호선 정보", items: [
+                        .init(cellTitle: "열차의 행선지를 입력해주세요.", cellData: "인천행", type: .TextField, focus: false),
+                        .init(cellTitle: "현재 역을 입력해주세요.", cellData: "시청역", type: .TextField, focus: false),
+                        .init(cellTitle: "현재 역이 청량리 ~ 서울역 안에 있나요?", cellData: "", type: .TwoBtn, focus: false)
+                    ])
+                ]),
+                .next(8,[
+                    ReportTableViewCellSection(sectionName: "민원 호선", items: [.init(cellTitle: "몇호선 민원을 접수하시겠어요?", cellData: "", type: .Line, focus: false)]),
+                    ReportTableViewCellSection(sectionName: "호선 정보", items: [
+                        .init(cellTitle: "열차의 행선지를 입력해주세요.", cellData: "인천행", type: .TextField, focus: false),
+                        .init(cellTitle: "현재 역을 입력해주세요.", cellData: "시청역", type: .TextField, focus: false),
+                        .init(cellTitle: "현재 역이 청량리 ~ 서울역 안에 있나요?", cellData: "Y", type: .TwoBtn, focus: false)
+                    ]),
+                    ReportTableViewCellSection(sectionName: "상세 정보", items: [
+                        .init(cellTitle: "칸 위치나 열차번호를 입력해주세요.", cellData: "", type: .TextField, focus: true)])
+                ])
+            ])
+        )
+    }
+    
+    func testThree(){
+        // GIVEN
+        let scheduler = TestScheduler(initialClock: 0)
+        let nowData = BehaviorRelay<[ReportTableViewCellSection]>(value: [])
+        let observer = scheduler.createObserver([ReportTableViewCellSection].self)
+        
+        nowData
+            .subscribe(observer)
+            .disposed(by: self.bag)
+        
+        let step = scheduler.createHotObservable([
+            .next(3, [
+                ReportTableViewCellSection(sectionName: "민원 호선", items: [.init(cellTitle: "몇호선 민원을 접수하시겠어요?", cellData: "", type: .Line, focus: false)]),
+                ReportTableViewCellSection(sectionName: "호선 정보", items: [
+                    .init(cellTitle: "열차의 행선지를 입력해주세요.", cellData: "인천행", type: .TextField, focus: false),
+                    .init(cellTitle: "현재 역을 입력해주세요.", cellData: "시청역", type: .TextField, focus: false),
+                    .init(cellTitle: "현재 역이 청량리 ~ 서울역 안에 있나요?", cellData: "Y", type: .TwoBtn, focus: false)
+                ]),
+                ReportTableViewCellSection(sectionName: "상세 정보", items: [
+                    .init(cellTitle: "칸 위치나 열차번호를 입력해주세요.", cellData: "", type: .TextField, focus: true)])
+            ])
+        ])
+        
+        step
+            .bind(to: nowData)
+            .disposed(by: self.bag)
+        
+        // WHEN
+        // 열차정보
+        let trainCar = PublishSubject<String>()
+        let trainCarEvent = scheduler.createHotObservable([
+            .next(7, "1-4")
+        ])
+        
+        trainCarEvent
+            .subscribe(trainCar)
+            .disposed(by: self.bag)
+        
+        trainCar
+            .map{[weak self] data in
+                var now = nowData.value
+                now = self!.model.cellDataSave(nowData: now, data: data, index: IndexPath(row: 0, section: 2))
+                
+                return now
+            }
+            .bind(to: nowData)
+            .disposed(by: self.bag)
+        
+        scheduler.start()
+        
+        // THEN
+        expect(observer.events).to(
+            equal([
+                .next(0, []),
+                .next(3, [
+                    ReportTableViewCellSection(sectionName: "민원 호선", items: [.init(cellTitle: "몇호선 민원을 접수하시겠어요?", cellData: "", type: .Line, focus: false)]),
+                    ReportTableViewCellSection(sectionName: "호선 정보", items: [
+                        .init(cellTitle: "열차의 행선지를 입력해주세요.", cellData: "인천행", type: .TextField, focus: false),
+                        .init(cellTitle: "현재 역을 입력해주세요.", cellData: "시청역", type: .TextField, focus: false),
+                        .init(cellTitle: "현재 역이 청량리 ~ 서울역 안에 있나요?", cellData: "Y", type: .TwoBtn, focus: false)
+                    ]),
+                    ReportTableViewCellSection(sectionName: "상세 정보", items: [
+                        .init(cellTitle: "칸 위치나 열차번호를 입력해주세요.", cellData: "", type: .TextField, focus: true)])
+                ]),
+                .next(7, [
+                    ReportTableViewCellSection(sectionName: "민원 호선", items: [.init(cellTitle: "몇호선 민원을 접수하시겠어요?", cellData: "", type: .Line, focus: false)]),
+                    ReportTableViewCellSection(sectionName: "호선 정보", items: [
+                        .init(cellTitle: "열차의 행선지를 입력해주세요.", cellData: "인천행", type: .TextField, focus: false),
+                        .init(cellTitle: "현재 역을 입력해주세요.", cellData: "시청역", type: .TextField, focus: false),
+                        .init(cellTitle: "현재 역이 청량리 ~ 서울역 안에 있나요?", cellData: "Y", type: .TwoBtn, focus: false)
+                    ]),
+                    ReportTableViewCellSection(sectionName: "상세 정보", items: [
+                        .init(cellTitle: "칸 위치나 열차번호를 입력해주세요.", cellData: "1-4", type: .TextField, focus: false)])
+                ])
+            ])
+        )
+    }
+    
+    func testTotalStep(){
+        // GIVEN
+        let scheduler = TestScheduler(initialClock: 0)
+        let nowData = PublishRelay<ReportMSGData>()
+        let observer = scheduler.createObserver(ReportMSGData.self)
+        
+        nowData
+            .subscribe(observer)
+            .disposed(by: self.bag)
+        
+        // WHEN
+        // line
+        let line = PublishSubject<ReportBrandData>()
+        let lineEvnet = scheduler.createHotObservable([
+            .next(1, ReportBrandData.one)
+        ])
+        
+        lineEvnet
+            .subscribe(line)
+            .disposed(by: self.bag)
+        
+        // nowStation
+        let nowStation = PublishSubject<String>()
+        let nowStationEvent = scheduler.createHotObservable([
+            .next(3, "시청역")
+        ])
+        
+        nowStationEvent
+            .subscribe(nowStation)
+            .disposed(by: self.bag)
+        
+        // destination
+        let destination = PublishSubject<String>()
+        let destinationEvent = scheduler.createHotObservable([
+            .next(5, "인천행")
+        ])
+        
+        destinationEvent
+            .subscribe(destination)
+            .disposed(by: self.bag)
+        
+        // brand
+        let brand = PublishSubject<String>()
+        let brandEvent = scheduler.createHotObservable([
+            .next(8, "Y")
+        ])
+        
+        brandEvent
+            .subscribe(brand)
+            .disposed(by: self.bag)
+        
+        // trainCar
+        let trainCar = PublishSubject<String>()
+        let trainCarEvent = scheduler.createHotObservable([
+            .next(10, "1-4")
+        ])
+        
+        trainCarEvent
+            .subscribe(trainCar)
+            .disposed(by: self.bag)
+        
+        Observable.combineLatest(line, nowStation, destination, trainCar, brand){ line, station, de, train, subwayBrand in
+            var brand = subwayBrand
+            
+            if line != .one || line != .three || line != .four{
+                if subwayBrand == "N/A"{
+                    brand = "N"
+                }
+            }
+           
+            return ReportMSGData(line: line, nowStation: station, destination: de, trainCar: train, contants: "", brand: brand)
+        }
+        .bind(to: nowData)
+        .disposed(by: self.bag)
+        
+        scheduler.start()
+        
+        // THEN
+        expect(observer.events).to(
+            equal([
+                .next(10, ReportMSGData(line: .one, nowStation: "시청역", destination: "인천행", trainCar: "1-4", contants: "", brand: "Y"))
+            ])
+        )
+    }
+}
