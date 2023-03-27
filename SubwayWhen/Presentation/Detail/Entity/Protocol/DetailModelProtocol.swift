5e5d623a6b46e9706049e984346eb523fe585aa4
YoonSuLee <ysleedev01@gmail.com>
2023-03-27 12:20:29 +0900

    Refactor(DetailVC): MainTableViewCellData를 쓰지 않고 자체적인 DetailLoadData타입을 쓰도록 변경

--- SubwayWhen/Presentation/Detail/Entity/Protocol/DetailModelProtocol.swift
+++ SubwayWhen/Presentation/Detail/Entity/Protocol/DetailModelProtocol.swift
@@ -11,9 +11,9 @@ import RxSwift
 
 protocol DetailModelProtocol{
     func nextAndBackStationSearch(backId : String, nextId : String) -> [String]
-    func mainCellDataToScheduleSearch(_ item : MainTableViewCellData) -> ScheduleSearch
-    func mainCellDataToDetailSection(_ data : MainTableViewCellData) -> [DetailTableViewSectionData]
+    func mainCellDataToScheduleSearch(_ item : DetailLoadData) -> ScheduleSearch
+    func mainCellDataToDetailSection(_ data : DetailLoadData) -> [DetailTableViewSectionData]
     func scheduleLoad(_ data : ScheduleSearch) -> Observable<[ResultSchdule]>
     func arrvialDataLoad(_ station : String) -> Observable<[RealtimeStationArrival]>
-    func arrivalDataMatching(station : MainTableViewCellData, arrivalData : [RealtimeStationArrival]) -> [RealtimeStationArrival]
+    func arrivalDataMatching(station : DetailLoadData, arrivalData : [RealtimeStationArrival]) -> [RealtimeStationArrival]
 }
