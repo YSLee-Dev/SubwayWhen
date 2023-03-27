6bbbf9037e7da7dc6d24d5ebd6d39ae177eb7140
YoonSuLee <ysleedev01@gmail.com>
2023-03-27 21:34:05 +0900

    Test(ReportVC): ReportChecModalModel, ReportContentsModalModel 테스트 코드 추가

---
+++ SubwayWhenTests/Report/ReportCheckModalModelTests.swift
@@ -0,0 +1,119 @@
+//
+//  ReportCheckModalModelTests.swift
+//  SubwayWhenTests
+//
+//  Created by 이윤수 on 2023/03/27.
+//
+
+import XCTest
+
+import RxSwift
+import RxBlocking
+import Nimble
+
+@testable import SubwayWhen
+
+final class ReportCheckModalModelTests: XCTestCase {
+    var model : ReportCheckModalModelProtocol!
+    var msgData : ReportMSGData!
+    
+    override func setUp() {
+        self.model = ReportCheckModalModel()
+        self.msgData = ReportMSGData(line: .one, nowStation: "시청", destination: "인천", trainCar: "1-4", contants: "너무 더워요.", brand: "Y")
+    }
+    
+    func testCreateMsg(){
+        // GIVEN
+        let hour = 1
+        let minute = 10
+        let data = self.model.createMsg(nowHour: hour, nowMinute: minute, data: self.msgData)
+        
+        // WHEN
+        let requestData = data
+        let dummyData =  """
+                    호선: \(self.msgData.line.rawValue)
+                    행선지: \(self.msgData.destination)행
+                    열차번호(칸 위치): \(self.msgData.trainCar)
+                    현재위치: (\(hour):\(minute)) \(self.msgData.nowStation)역
+                    \(self.msgData.contants)
+                    """
+        
+        // THEN
+        expect(requestData).to(
+            equal(dummyData),
+            description: "같은 데이터이기 때문에 값이 같아야함"
+        )
+    }
+    
+    func testNumberMatching(){
+        // GIVEN
+        let threeMSGData = ReportMSGData(line: .three, nowStation: "", destination: "", trainCar: "", contants: "", brand: "Y")
+        let sevenMSGData = ReportMSGData(line: .seven, nowStation: "", destination: "", trainCar: "", contants: "", brand: "N")
+        let shinMSGData = ReportMSGData(line: .shinbundang, nowStation: "", destination: "", trainCar: "", contants: "", brand: "N")
+        let gyeonguiMSGData = ReportMSGData(line: .gyeongui, nowStation: "", destination: "", trainCar: "", contants: "", brand: "N")
+        
+        let oneData = self.model.numberMatching(data: self.msgData)
+        let threeData = self.model.numberMatching(data: threeMSGData)
+        let sevenData = self.model.numberMatching(data: sevenMSGData)
+        let shinData = self.model.numberMatching(data: shinMSGData)
+        let gyeonguiData = self.model.numberMatching(data: gyeonguiMSGData)
+        
+        // WHEN
+        let requestNumber = oneData
+        let dummyData = "1577-1234"
+        
+        let requestThreeNumber = threeData
+        let dummyThreeData = "1544-7769"
+        
+        let requestSevenNumber = sevenData
+        let dummySevenData = "1577-1234"
+        
+        let requestShinNumber = shinData
+        let dummyShinData = "031-8018-7777"
+        
+        let requestGyeonguiNumber = gyeonguiData
+        let dummyGyeonguiData = "1544-7769"
+        
+        // THEN
+        expect(requestNumber).to(
+            equal(dummyData),
+            description: "1호선이지만, brand가 Y이기 때문에 서울교통공사 연락처가 나와야함"
+        )
+        
+        expect(requestThreeNumber).to(
+            equal(dummyThreeData),
+            description: "3호선이지만, brand가 Y이기 때문에 코레일 연락처가 나와야함"
+        )
+        
+        expect(requestSevenNumber).to(
+            equal(dummySevenData),
+            description: "7호선이면 서울교통공사 연락처가 나와야함"
+        )
+        
+        expect(requestShinNumber).to(
+            equal(dummyShinData),
+            description: "신분당선이면, 신분당선 연락처가 나와야함"
+        )
+        
+        expect(requestGyeonguiNumber).to(
+            equal(dummyGyeonguiData),
+            description: "경의중앙선이면 코레일 연락처가 나와야함"
+        )
+    }
+    
+    func testNumberMatchingError(){
+        // GIVEN
+        let msgData = ReportMSGData(line: .not, nowStation: "", destination: "", trainCar: "", contants: "", brand: "N")
+        let data = self.model.numberMatching(data: msgData)
+        
+        // WHEN
+        let requestNumber = data
+        let dummyData = ""
+        
+        // THEN
+        expect(requestNumber).to(
+            equal(dummyData),
+            description: "노선이 없으면 공백이 나와야함"
+        )
+    }
+}
---
+++ SubwayWhenTests/Report/ReportContentsModalModelTests.swift
@@ -0,0 +1,48 @@
+//
+//  ReportContentsModalModelTests.swift
+//  SubwayWhenTests
+//
+//  Created by 이윤수 on 2023/03/27.
+//
+
+import XCTest
+
+import RxSwift
+import RxBlocking
+import Nimble
+
+@testable import SubwayWhen
+
+final class ReportContentsModalModelTests: XCTestCase {
+    var model : ReportContentsModalModelProtocol!
+
+    override func setUp() {
+        self.model = ReportContentsModalModel()
+    }
+    
+    func testReportList(){
+        // GIVEN
+        let data = self.model.reportList()
+        let blocking = data.toBlocking()
+        let arrayData = try! blocking.toArray().first
+        
+        // WHEN
+        let requestCount = arrayData?.first?.items.count
+        let duumyCount = 9
+        
+        let requestFirst = arrayData?.first?.items.first?.contents
+        let dummyFirst = "차내 온도가 높습니다. 에어컨 조절 요청드립니다."
+        
+        // THEN
+        expect(requestCount).to(
+            equal(duumyCount),
+            description: "count는 9으로 같아야함"
+        )
+        
+        expect(requestFirst).to(
+            equal(dummyFirst),
+            description: "데이터가 동일하기 때문에 첫 번째 데이터의 값도 동일해야함"
+        )
+    }
+    
+}
