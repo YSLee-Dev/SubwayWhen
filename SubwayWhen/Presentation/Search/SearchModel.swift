dd2f92014425f4570e42fee5ae1cf75368350288
YoonSuLee <ysleedev01@gmail.com>
2023-03-28 11:57:27 +0900

    Refactor(SearchVC): SearchModel <-> SearchViewModel간의 의존성(DI) 문제 해결

--- SubwayWhen/Presentation/Search/SearchModel.swift
+++ SubwayWhen/Presentation/Search/SearchModel.swift
@@ -11,7 +11,7 @@ import RxSwift
 import RxCocoa
 import FirebaseDatabase
 
-class SearchModel{
+class SearchModel : SearchModelProtocol{
     let totalLoadModel : TotalLoadProtocol
     
     init(
@@ -20,8 +20,27 @@ class SearchModel{
         self.totalLoadModel = model
     }
     
-    // 지하철 역명 통신
-   
+    func fireBaseDefaultViewListLoad() -> Observable<[String]>{
+        self.totalLoadModel.defaultViewListLoad()
+    }
+    
+    func stationNameSearchRequest(_ name : String) -> Observable<SearchStaion>{
+        self.totalLoadModel.stationNameSearchReponse(name)
+    }
+    
+    func stationNameMatching(_ searchData : SearchStaion) -> [ResultVCSection]{
+        
+        let data = searchData.SearchInfoBySubwayNameService.row
+        // 기본 값 제거
+        if data.count == 5 && data[0].stationName == "용답"{
+            return []
+        }else{
+            let section = data.map{
+                return ResultVCSection(section: "\($0.stationCode)\($0.lineCode)", items: [ResultVCCellData(stationName: $0.stationName, lineNumber: $0.lineNumber.rawValue, stationCode: $0.stationCode, useLine: $0.useLine, lineCode: $0.lineCode)])
+            }
+            return section
+        }
+    }
     
   
 }
