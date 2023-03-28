2309a69b54f7e84aaf2fa5c997b4fd187868f083
YoonSuLee <ysleedev01@gmail.com>
2023-03-28 19:54:24 +0900

    Refactor(SearchVC): ViewModel <-> VC간의 의존성 문제(DI) 해결

---
+++ SubwayWhen/Presentation/Search/Modal/Entity/Protocol/ModalViewModelProtocol.swift
@@ -0,0 +1,31 @@
+//
+//  ModalViewModelProtocol.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 2023/03/28.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+protocol ModalViewModelProtocol{
+    var modalData : Driver<ResultVCCellData>{get}
+    var modalClose : Driver<Void>{get}
+    var alertShow : Driver<Void>{get}
+    var saveComplete : Driver<Void>{get}
+    var disposableDetailMove : Driver<DetailLoadData>{get}
+    
+    // INPUT
+    var overlapOkBtnTap : PublishRelay<Void>{get}
+    var clickCellData : PublishRelay<ResultVCCellData>{get}
+    var upDownBtnClick : PublishRelay<Bool>{get}
+    var notService : PublishRelay<Void>{get}
+    var groupClick : BehaviorRelay<SaveStationGroup>{get}
+    var exceptionLastStationText: BehaviorRelay<String?>{get}
+    var disposableBtnTap : PublishRelay<Bool>{get}
+    
+    var modalCloseEvent : PublishRelay<Bool>{get}
+    var mainCellData : PublishRelay<DetailLoadData>{get}
+}
