f7963d2351cae150fe412b12acb50b93a7ca7026
YoonSuLee <ysleedev01@gmail.com>
2023-03-29 12:23:52 +0900

    Feat(SettingVC): 오픈 라이선스 열람 기능 추가

--- SubwayWhen/Presentation/Setting/Coordinator/SettingCoordinator.swift
+++ SubwayWhen/Presentation/Setting/Coordinator/SettingCoordinator.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+
+import AcknowList
  
 class SettingCoordinator : Coordinator{
     weak var parentCoordinator : Coordinator?
@@ -19,7 +21,19 @@ class SettingCoordinator : Coordinator{
     func start() {
         let settingVC = SettingVC(viewModel: SettingViewModel())
         settingVC.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "gearshape"), tag: 2)
+        settingVC.delegate = self
+        
         self.naviagation.pushViewController(settingVC, animated: true)
     }
     
+}
+
+extension SettingCoordinator : SettingVCDelegate{
+    func licensesTap() {
+        let vc = AcknowListViewController(fileNamed: "Pods-SubwayWhen-acknowledgements")
+        vc.hidesBottomBarWhenPushed = true
+        vc.headerText = "SubwayWhen Licenses"
+        vc.footerText = "YSLee-Dev"
+        self.naviagation.pushViewController(vc, animated: true)
+    }
 }
