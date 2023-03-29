5ddd101576e1f147be618ab60a67f6c17da75f76
YoonSuLee <ysleedev01@gmail.com>
2023-03-29 21:13:54 +0900

    Fix(MainVC): MainVC에서 SearchVC로 넘어갈 때 코디네이터를 통해 넘어가도록 변경

--- SubwayWhen/Presentation/Main/Coordinator/Protocol/MainDelegate.swift
+++ SubwayWhen/Presentation/Main/Coordinator/Protocol/MainDelegate.swift
@@ -10,4 +10,5 @@ import Foundation
 protocol MainDelegate : AnyObject {
     func pushTap(action : MainCoordinatorAction)
     func pushDetailTap(data : MainTableViewCellData)
+    func plusStationTap()
 }
