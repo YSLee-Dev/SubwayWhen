f71c86d91b15caa8c557f2a69e519debe86d5d49
YoonSuLee <ysleedev01@gmail.com>
2023-05-16 10:19:28 +0900

    Fix(App): 백그라운드에서 포그라운드 진입 시 업데이트 제거

--- SubwayWhen/Application/AppDelegate.swift
+++ SubwayWhen/Application/AppDelegate.swift
@@ -11,9 +11,16 @@ import BackgroundTasks
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var updateCount = 0
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        // Background 작업
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.yslee.subwaywhen-liveactivity.refresh", using: nil){task in
+            self.backgroundLiveActivityUpdate(task: task as! BGAppRefreshTask)
+        }
         
         // 네트워크 감지 class
         NetworkMonitor.shared.monitorStart()
@@ -35,5 +42,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         NetworkMonitor.shared.monitorStop()
         SubwayWhenDetailWidgetManager.shared.stop()
+    }
+}
+
+extension AppDelegate{
+    func backgroundLiveActivityUpdate(task : BGAppRefreshTask){
+        if self.updateCount >= 10{
+            self.scheduleAppRefresh()
+            
+            SubwayWhenDetailWidgetManager.shared.update(status: "TEST", statusMSG: "TEST", nowStation: "TEST", lastUpdate: "TEST")
+            self.updateCount += 1
+            
+            task.expirationHandler = {
+                task.setTaskCompleted(success: false)
+            }
+        }else{
+            task.setTaskCompleted(success: true)
+        }
+    }
+    
+    func scheduleAppRefresh() {
+        let request = BGAppRefreshTaskRequest(identifier: "com.yslee.subwaywhen-liveactivity.refresh")
+        request.earliestBeginDate = Date(timeIntervalSinceNow: 15)
+        
+        do {
+            try BGTaskScheduler.shared.submit(request)
+        } catch {
+            print("NOT BACKGROUND SCHEDULE SUBMIT")
+        }
     }
 }
