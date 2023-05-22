0edf33fda3fbcd8095fcb73c2b2b746b87fb5a1e
YoonSuLee <ysleedev01@gmail.com>
2023-05-22 18:23:40 +0900

    Feat(App): 백그라운드 통신 제거

--- SubwayWhen/Application/SceneDelegate.swift
+++ SubwayWhen/Application/SceneDelegate.swift
@@ -14,7 +14,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var model = AppDefaultModel()
     
-    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
@@ -46,11 +45,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // 팝업 로드
         self.appDefaultManager?.popup()
         
-    }
-    
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
-        appDelegate.scheduleAppRefresh()
     }
 }
 
