78d7c3079d146f9bae8649518d05ff171592af7a
YoonSuLee <ysleedev01@gmail.com>
2023-06-21 21:32:01 +0900

    Feat(SettingVC): SettingNotiModalVC에서 지하철 역을 추가하는 화면 추가

--- SubwayWhen/Presentation/Setting/SettingNotiModal/Coordinator/SettingNotiCoordinator.swift
+++ SubwayWhen/Presentation/Setting/SettingNotiModal/Coordinator/SettingNotiCoordinator.swift
@@ -10,7 +10,7 @@ import UIKit
 class SettingNotiCoordinator: Coordinator {
     var childCoordinator: [Coordinator] = []
     let rootVC: UINavigationController
-    let naviagation: UINavigationController
+    var naviagation: UINavigationController?
     
     weak var delegate: SettingNotiCoordinatorProtocol?
     
@@ -18,7 +18,6 @@ class SettingNotiCoordinator: Coordinator {
         rootVC: UINavigationController
     ) {
         self.rootVC = rootVC
-        self.naviagation = UINavigationController()
     }
     
     func start() {
@@ -31,18 +30,45 @@ class SettingNotiCoordinator: Coordinator {
             subTitle: "출퇴근 시간에 맞게 정해놓은 지하철역으로 알림을 주는 기능에요.",
             viewModel: viewModel
         )
-        modal.modalPresentationStyle = .overFullScreen
         
-        self.rootVC.present(modal, animated: false)
+        self.naviagation = UINavigationController(rootViewController: modal)
+        
+        guard let naviagation = self.naviagation else {return}
+        naviagation.modalPresentationStyle = .overFullScreen
+        
+        self.rootVC.present(naviagation, animated: false)
     }
 }
 
 extension SettingNotiCoordinator: SettingNotiModalVCAction {
+    func stationTap(type: Bool) {
+        guard let naviagation = self.naviagation else {return}
+        let selectModalCoordinator = SettingNotiSelectCoordinator(navigation: naviagation)
+        selectModalCoordinator.delegate = self
+        selectModalCoordinator.start()
+      
+        self.childCoordinator.append(selectModalCoordinator)
+    }
+    
     func didDisappear() {
-        self.delegate?.didDisappear(settingNotiCoordinator: self)
+        if self.childCoordinator.isEmpty {
+            self.delegate?.didDisappear(settingNotiCoordinator: self)
+        }
     }
     
     func dismiss() {
         self.delegate?.dismiss()
+    }
+}
+
+extension SettingNotiCoordinator: SettingNotiSelectModalCoordinatorProtocol {
+    func didDisappear(settingNotiSelectModalCoordinator: Coordinator) {
+        self.childCoordinator = self.childCoordinator.filter{
+            $0 !== settingNotiSelectModalCoordinator
+        }
+    }
+    
+    func pop() {
+        self.naviagation?.popViewController(animated: true)
     }
 }
