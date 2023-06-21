623333bdddaa356f51c2eb0bbfdcd42525f01c61
YoonSuLee <ysleedev01@gmail.com>
2023-06-21 20:04:35 +0900

    Refactor(SettingVC): SettingNotiModalVC는 Coordiantor 패턴을 이용하도록 변경

---
+++ SubwayWhen/Presentation/Setting/SettingNotiModal/Coordinator/Protocol/SettingNotiModalVCAction.swift
@@ -0,0 +1,13 @@
+//
+//  SettingNotiModalVCAction.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 2023/06/21.
+//
+
+import Foundation
+
+protocol SettingNotiModalVCAction: AnyObject {
+    func didDisappear()
+    func dismiss()
+}
