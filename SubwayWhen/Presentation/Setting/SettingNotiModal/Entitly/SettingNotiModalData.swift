3457d5107236a76056866a8f51f61e5069e0fc0a
YoonSuLee <ysleedev01@gmail.com>
2023-06-26 20:19:48 +0900

    Feat(SettingVC): SettingNotiModalVC에서 선택된 역을 반영하는 기능 추가

---
+++ SubwayWhen/Presentation/Setting/SettingNotiModal/Entitly/SettingNotiModalData.swift
@@ -0,0 +1,16 @@
+//
+//  SettingNotiModalData.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 2023/06/26.
+//
+
+import Foundation
+
+struct SettingNotiModalData {
+    let id: String
+    let stationName: String
+    let useLine: String
+    let line: String
+    let group: SaveStationGroup
+}
