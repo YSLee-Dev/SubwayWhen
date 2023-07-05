d67d134f3f17c2367468b1f43f59261666448033
YoonSuLee <ysleedev01@gmail.com>
2023-07-05 11:25:54 +0900

    Feat(SearchVC): LocationModal Coordinator, ViewModel, VC 추가

---
+++ SubwayWhen/Presentation/Search/Location/Coordinator/Protocol/LocationModalCoordinatorProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  LocationModalCoordinatorProtocol.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 2023/07/05.
+//
+
+import Foundation
+
+protocol LocationModalCoordinatorProtocol: AnyObject {
+    func dismiss()
+    func didDisappear(locationModalCoordinator: Coordinator)
+}
