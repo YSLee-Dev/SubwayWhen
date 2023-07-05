1c5a4d97fa7d1a697336d4521b4a6d6155774a76
YoonSuLee <ysleedev01@gmail.com>
2023-07-05 11:25:54 +0900

    Feat(SearchVC): LocationModal Coordinator, ViewModel, VC 추가

--- SubwayWhen/Presentation/Search/SearchViewModel.swift
+++ SubwayWhen/Presentation/Search/SearchViewModel.swift
@@ -101,5 +101,12 @@ class SearchViewModel : SearchViewModelProtocol{
             }
             .bind(to: self.nowData)
             .disposed(by: self.bag)
+        
+        self.defaultViewModel.locationBtnTap
+            .withUnretained(self)
+            .subscribe(onNext: { viewModel, _ in
+                viewModel.delegate?.locationPresent()
+            })
+            .disposed(by: self.bag)
     }
 }
