2d67a1a1ab2de3009dacb60a63814e308c55dfda
YoonSuLee <ysleedev01@gmail.com>
2023-09-20 11:38:39 +0900

    Fix(App): 간혹 네트워크 연결이 되어있음에도 불구하고 네트워크 오류 창이 뜨던 오류 수정

--- SubwayWhen/Application/AppDelegate.swift
+++ SubwayWhen/Application/AppDelegate.swift
@@ -16,9 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         
-        // 네트워크 감지 class
-        NetworkMonitor.shared.monitorStart()
-        
         // LanchScreen 지연
         sleep(1)
         
@@ -27,6 +24,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // 포그라운드에서 노티 받기
         UNUserNotificationCenter.current().delegate = self
+        
+        // 네트워크 감지 class
+        NetworkMonitor.shared.monitorStart()
+        
         
         return true
     }
@@ -38,7 +39,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
-        print("applicationWillTerminate")
         SubwayWhenDetailWidgetManager.shared.stop()
     }
 }
