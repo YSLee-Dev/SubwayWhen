4e6ff40117a563d4ea790cf6d33fa03fdcf1bf46
YSLee-Dev <ysleedev01@gmail.com>
2023-09-24 20:10:23 +0900

    Feat(TutorialVC): VC 디자인 구성

--- SubwayWhen/Presentation/Tutorial/TutorialVC.swift
+++ SubwayWhen/Presentation/Tutorial/TutorialVC.swift
@@ -16,17 +16,24 @@ class TutorialVC: UIViewController {
         let mainBg = MainStyleUIView()
         
         let title = UILabel()
-        title.font = .boldSystemFont(ofSize: ViewStyle.FontSize.mainTitleSize)
+        title.font = .boldSystemFont(ofSize: ViewStyle.FontSize.largeSize)
         title.textAlignment = .left
         title.adjustsFontSizeToFitWidth = true
+        title.numberOfLines = 0
         title.text = "다음 버튼을 누르면, 민실씨의 기능을 확인할 수 있어요."
         
         mainBg.addSubview(title)
         title.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.edges.equalToSuperview().inset(16)
         }
         return mainBg
     }()
+    
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init()).then {
+        $0.backgroundColor = .systemBackground
+        $0.delegate = nil
+        $0.dataSource = nil
+    }
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -46,22 +53,46 @@ extension TutorialVC {
     private func attribute() {
         self.view.backgroundColor = .systemBackground
         self.mainTitle.mainTitleLabel.text = "지하철 민실씨를 \n설치해주셔서감사합니다."
+        
+        self.collectionView.collectionViewLayout = self.collectionViewLayout()
     }
     
     private func layout() {
-        [self.mainTitle, self.subTitle].forEach {
+        [self.mainTitle, self.subTitle, self.collectionView].forEach {
             self.view.addSubview($0)
         }
         
         self.mainTitle.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview()
-            $0.top.equalTo(self.view.safeAreaLayoutGuide)
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).inset(45)
             $0.height.equalTo(45)
         }
         self.subTitle.snp.makeConstraints{
-            $0.leading.trailing.equalToSuperview()
-            $0.top.equalTo(self.mainTitle.snp.bottom)
-            $0.height.equalTo(30)
+            $0.leading.trailing.equalToSuperview().inset(ViewStyle.padding.mainStyleViewLR)
+            $0.top.equalTo(self.mainTitle.snp.bottom).offset(20)
         }
+        self.collectionView.snp.makeConstraints {
+            $0.leading.trailing.equalTo(self.subTitle)
+            $0.top.equalTo(self.subTitle.snp.bottom).offset(20)
+            $0.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(20)
+        }
+    }
+    
+    private func collectionViewLayout() -> UICollectionViewLayout{
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutDecorationItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 8, leading: 8, bottom: 8, trailing: 8)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, repeatingSubitem: item, count: 1)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPagingCentered
+        
+        return UICollectionViewCompositionalLayout(section: section)
     }
 }
