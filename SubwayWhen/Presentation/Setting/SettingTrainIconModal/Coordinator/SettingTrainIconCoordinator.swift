68ce656f7023227cf0a3d95a174a6a2ad4f1c3a0
YSLee-Dev <ysleedev01@gmail.com>
2024-02-09 15:06:19 +0900

    Feat(SettingVC): 열차 아이콘 변경 관련 VC, ViewModel, Coordinator 생성 및 코드 최적화

---
+++ SubwayWhen/Presentation/Setting/SettingTrainIconModal/Coordinator/SettingTrainIconCoordinator.swift
@@ -0,0 +1,46 @@
+//
+//  SettingTrainIconModalCoordinator.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 2/9/24.
+//
+
+import UIKit
+
+class SettingTrainIconModalCoordinator: Coordinator {
+    private let navigation: UINavigationController
+    var childCoordinator: [Coordinator] = []
+    
+    weak var delegate: SettingTrainIconCoordinatorProtocol?
+    
+    init(
+        navigation: UINavigationController
+    ) {
+        self.navigation = navigation
+    }
+    
+    func start() {
+        let viewModel = SettingTrainIconModalViewModel()
+        viewModel.delegate = self
+        
+        let vc = SettingTrainIconModalVC(
+            modalHeight: 405,
+            btnTitle: "저장",
+            mainTitle: "열차 아이콘",
+            subTitle: "상세화면의 열차 아이콘을 변경하는 기능이에요.",
+            viewModel: viewModel
+        )
+        vc.modalPresentationStyle = .overFullScreen
+        self.navigation.present(vc, animated: false)
+    }
+}
+
+extension SettingTrainIconModalCoordinator: SettingTrainIconModalVCAction {
+    func didDisappear() {
+        self.delegate?.didDisappear(coordinator: self)
+    }
+    
+    func dismiss() {
+        self.delegate?.dismiss()
+    }
+}
