521eb7b25551009c3824ca603ea4fbcc8ac095c2
YSLee-Dev <ysleedev01@gmail.com>
2024-03-28 17:50:32 +0900

    Feat(Widget): 위젯 Target 추가

---
+++ SubwayWhenHomeWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  SubwayWhenHomeWidget
+//
+//  Created by 이윤수 on 3/28/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
