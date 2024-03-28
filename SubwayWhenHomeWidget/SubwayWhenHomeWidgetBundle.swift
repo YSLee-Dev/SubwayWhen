189a745b1d9d5b98c367f2f242abd9c51beae3fd
YSLee-Dev <ysleedev01@gmail.com>
2024-03-28 17:50:32 +0900

    Feat(Widget): 위젯 Target 추가

---
+++ SubwayWhenHomeWidget/SubwayWhenHomeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SubwayWhenHomeWidgetBundle.swift
+//  SubwayWhenHomeWidget
+//
+//  Created by 이윤수 on 3/28/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SubwayWhenHomeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SubwayWhenHomeWidget()
+    }
+}
