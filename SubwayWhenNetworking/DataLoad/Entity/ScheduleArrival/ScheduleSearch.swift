c503fb02a2aee722498c620f9198c9bbdb4b6a3f
YSLee-Dev <ysleedev01@gmail.com>
2024-04-19 13:00:58 +0900

    Fix(DataLoad): 시간표를 지원하지 않는 노선 추가

--- SubwayWhenNetworking/DataLoad/Entity/ScheduleArrival/ScheduleSearch.swift
+++ SubwayWhenNetworking/DataLoad/Entity/ScheduleArrival/ScheduleSearch.swift
@@ -15,7 +15,7 @@ struct ScheduleSearch{
     let korailCode : String
     
     var lineScheduleType: ScheduleType {
-        if  line == "신분당선" || line == "공항철도" || line == "우이신설경전철" || line == "" {
+        if  line == "신분당선" || line == "공항철도" || line == "우이신설경전철" || line == "" || line == "경강선" || line == "서해선" || line == "GTX-A" {
             return .Unowned
         } else if line == "경의선" || line == "경춘선" || line == "수인분당선" {
             return .Korail
