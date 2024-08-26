5f742f0c8070404f5bb431d42e55ab4e974f4bed
YSLee-Dev <ysleedev01@gmail.com>
2024-08-26 09:22:39 +0900

    Feat(Common > SwiftUI): SwiftUI용 NavigationBar와 ScrollView를 합친 View 제작 (애니메이션 적용)

--- SubwayWhen/Presentation/Common/SwiftUI/OffsetScrollViewInSUI.swift
+++ SubwayWhen/Presentation/Common/SwiftUI/OffsetScrollViewInSUI.swift
@@ -5,4 +5,37 @@
 //  Created by 이윤수 on 8/26/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct OffsetScrollViewInSUI<Contents>: View where Contents: View {
+    private let contentsView:  () -> Contents
+    
+    init(@ViewBuilder  content: @escaping () -> Contents) {
+        self.contentsView = content
+    }
+    
+    var body: some View {
+        ScrollView {
+            self.scrollObservableView
+            self.contentsView()
+        }
+    }
+    
+    private var scrollObservableView: some View {
+        GeometryReader { geo in
+            let offsetY = geo.frame(in: .global).origin.y
+            Color.clear
+                .preference(key: ScrollOffsetKey.self, value: offsetY)
+        }
+        .frame(height: 0)
+    }
+}
+
+struct ScrollOffsetKey: PreferenceKey {
+    typealias Value = CGFloat
+    static var defaultValue: CGFloat = 0.0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value += nextValue()
+    }
+}
+
