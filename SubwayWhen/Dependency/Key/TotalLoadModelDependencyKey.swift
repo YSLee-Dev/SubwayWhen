12ed47d045d50fd5571b1eb0e909330f88308779
YSLee-Dev <ysleedev01@gmail.com>
2024-08-26 13:37:28 +0900

    Feat(Dependency): TCA 용 Dependency 추가 및 Dependency 등록

---
+++ SubwayWhen/Dependency/Key/TotalLoadModelDependencyKey.swift
@@ -0,0 +1,14 @@
+//
+//  TotalLoadModelDependencyKey.swift
+//  SubwayWhenNetworking
+//
+//  Created by 이윤수 on 8/26/24.
+//
+
+import Foundation
+import ComposableArchitecture
+
+enum TotalLoadModelDependencyKey: DependencyKey {
+    static var liveValue: TotalLoadProtocol = TotalLoadModel()
+    static var previewValue: any TotalLoadProtocol = TotalLoadModel()
+}
