bed6494c70ba0b6e947dd7021a14557b51953231
YSLee-Dev <ysleedev01@gmail.com>
2024-08-26 13:38:30 +0900

    Feat(DataLoad): async await 용 함수 추가 및 Model이 Equatable을 준수하도록 변경

--- SubwayWhenNetworking/DataLoad/Entity/LiveArrival/RealtimeStationArrival.swift
+++ SubwayWhenNetworking/DataLoad/Entity/LiveArrival/RealtimeStationArrival.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct RealtimeStationArrival : Decodable{
+struct RealtimeStationArrival : Decodable, Equatable {
     let upDown : String
     let arrivalTime : String
     let previousStation : String?
