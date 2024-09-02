3520f7c73302679dc3a1128a34fb927112c0841d
YSLee-Dev <ysleedev01@gmail.com>
2024-09-02 14:27:55 +0900

    Feat(Detail): 시간표 View에 현재 로딩 상태를 나타내는 프로퍼티 추가, 로딩을 나타내는 뷰 추가

--- SubwayWhen/Presentation/Detail/DetailScheduleView.swift
+++ SubwayWhen/Presentation/Detail/DetailScheduleView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct DetailScheduleView: View {
-    private let gridItem = [GridItem(.flexible()), GridItem(.flexible())]
+    private let gridItem = [GridItem(.flexible(), spacing: 10), GridItem(.flexible(), spacing: 10)]
     var scheduleDataList: [ResultSchdule]
     let stationInfo: DetailSendModel
+    var nowLoading: Bool
     let moreBtnTapped: () -> ()
     
     var body: some View {
@@ -26,7 +27,8 @@ struct DetailScheduleView: View {
             MainStyleViewInSUI {
                 VStack(spacing: 0) {
                     HStack {
-                        let title = FixInfo.saveSetting.detailAutoReload ? 
+                        let title = self.nowLoading ? "시간표를 가져오고 있어요" :
+                        FixInfo.saveSetting.detailAutoReload ?
                         (self.scheduleDataList.first == nil ? "시간표를 불러올 수 없어요." :
                             (self.scheduleDataList.first!.type == .Unowned ? "시간표를 지원하지 않는 노선이에요." :
                                 (self.scheduleDataList.first!.useArrTime == "정보없음" ? "시간표를 불러올 수 없어요" :
@@ -46,30 +48,40 @@ struct DetailScheduleView: View {
                                 .foregroundColor(.init(uiColor: .label))
                         })
                     }
-                    .padding(.bottom, 10)
+                    .padding(.bottom, 20)
                     
                     ScrollView {
-                        LazyVGrid(columns: self.gridItem) {
-                            ForEach(self.scheduleDataList, id: \.startTime) { data in
-                                HStack {
-                                    let isFast = data.isFast == "급행" ? "(급)" : ""
-                                    let title = data.startTime == "⚠️ 정보없음" ? "" : "⏱️ \(isFast)\(data.lastStation)행 \(data.useArrTime)"
-                                    Text(title)
-                                        .font(.system(size: ViewStyle.FontSize.smallSize, weight: .medium))
-                                        .foregroundColor(.white)
-                                        .padding(.leading, 5)
-                                    Spacer()
+                        if self.nowLoading {
+                            VStack {
+                                Spacer()
+                                ProgressView()
+                                    .tint(Color("AppIconColor"))
+                                Spacer()
+                            }
+                            .position(x: (UIScreen.main.bounds.width / 2) - 40, y: 100)
+                        } else {
+                            LazyVGrid(columns: self.gridItem) {
+                                ForEach(self.scheduleDataList, id: \.startTime) { data in
+                                    HStack {
+                                        let isFast = data.isFast == "급행" ? "(급)" : ""
+                                        let title = data.startTime == "⚠️ 정보없음" ? "" : "⏱️ \(isFast)\(data.lastStation)행 \(data.useArrTime)"
+                                        Text(title)
+                                            .font(.system(size: ViewStyle.FontSize.smallSize, weight: .medium))
+                                            .foregroundColor(.white)
+                                            .padding(.leading, 5)
+                                        Spacer()
+                                    }
+                                    .frame(height: 40)
+                                    .background {
+                                        RoundedRectangle(cornerRadius: 15)
+                                            .fill(Color("\(self.stationInfo.lineNumber)"))
+                                    }
+                                    .padding(EdgeInsets(top: 6, leading: 2.5, bottom: 6, trailing: 2.5))
                                 }
-                                .frame(height: 37.5)
-                                .background {
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .fill(Color("\(self.stationInfo.lineNumber)"))
-                                }
-                                .padding(5)
                             }
                         }
                     }
-                    .frame(height: 150)
+                    .frame(height: 200)
                 }
                 .padding(15)
             }
@@ -88,5 +100,5 @@ struct DetailScheduleView: View {
             .init(startTime: "05:33:00", type: .Seoul, isFast: "", startStation: "수서", lastStation: "구파발"),
             .init(startTime: "05:40:00", type: .Seoul, isFast: "", startStation: "수서", lastStation: "대화"),
             .init(startTime: "05:46:00", type: .Seoul, isFast: "", startStation: "오금", lastStation: "구파발")
-    ], stationInfo: .init(upDown: "상행", stationName: "340", lineNumber: "03호선", stationCode: "340", lineCode: "1003", exceptionLastStation: "", korailCode: "")){}
+    ], stationInfo: .init(upDown: "상행", stationName: "340", lineNumber: "03호선", stationCode: "340", lineCode: "1003", exceptionLastStation: "", korailCode: ""), nowLoading: false){}
 }
