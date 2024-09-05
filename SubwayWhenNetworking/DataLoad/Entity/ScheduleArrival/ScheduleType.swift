3d48c2461e923bee68f5fbca77af425ec3fce534
YSLee-Dev <ysleedev01@gmail.com>
2024-09-05 10:44:31 +0900

    Feat(DataLoad): TotalLoad에서 신분당선 데이터를 받아 async/await + ResultSchedule로 내뱉는 함수 구현

--- SubwayWhenNetworking/DataLoad/Entity/ScheduleArrival/ScheduleType.swift
+++ SubwayWhenNetworking/DataLoad/Entity/ScheduleArrival/ScheduleType.swift
@@ -10,10 +10,13 @@ import Foundation
 enum ScheduleType: Equatable{
     case Seoul
     case Korail
+    case Sinbundang
     case Unowned
     
     static func lineNumberScheduleType(line: String) -> ScheduleType {
-        if  line == "신분당선" || line == "공항철도" || line == "우이신설경전철" || line == "" || line == "경강선" || line == "서해선" || line == "GTX-A" {
+        if line == "신분당선" {
+            return .Sinbundang
+        } else if  line == "공항철도" || line == "우이신설경전철" || line == "" || line == "경강선" || line == "서해선" || line == "GTX-A" {
             return .Unowned
         } else if line == "경의선" || line == "경춘선" || line == "수인분당선" {
             return .Korail
