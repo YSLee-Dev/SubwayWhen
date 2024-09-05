bcf2b32d9fa4d65bf8a77f38ea70d2bf5a9e65bc
YSLee-Dev <ysleedev01@gmail.com>
2024-09-05 10:37:55 +0900

    Feat(DataLoad): 신분당선 시간표를 로드하는 로직 추가

---
+++ SubwayWhenNetworking/DataLoad/Entity/ScheduleArrival/Sinbundang/SinbundangScheduleModel.swift
@@ -0,0 +1,17 @@
+//
+//  SinbundangScheduleModel.swift
+//  SubwayWhenNetworking
+//
+//  Created by 이윤수 on 9/5/24.
+//
+
+import Foundation
+
+struct SinbundangScheduleModel: Equatable, Decodable {
+    let endStation: String
+    let startStation: String
+    let startTime: String
+    let stationName: String
+    let updown: String
+    let week: String
+}
