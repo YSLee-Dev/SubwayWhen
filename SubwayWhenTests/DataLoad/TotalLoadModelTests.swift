4c570e4e836ac85b9520c314e391ea2612629bef
YSLee-Dev <ysleedev01@gmail.com>
2024-09-19 14:01:08 +0900

    Test(DataLoad): singleLiveDataLoad 함수 검증 로직 추가, 기존 검증 로직 제거

--- SubwayWhenTests/DataLoad/TotalLoadModelTests.swift
+++ SubwayWhenTests/DataLoad/TotalLoadModelTests.swift
@@ -117,64 +117,76 @@ final class TotalLoadModelTests: XCTestCase {
         )
     }
     
-//    func testSingleLiveDataLoad(){
-//        // GIVEN
-//        let data = self.arrivalTotalLoadModel.singleLiveDataLoad(station: "교대")
-//        let blocking = data.toBlocking()
-//        let arrayData = try! blocking.toArray()
-//        
-//        // WHEN
-//        let requestStationName = arrayData.first?.realtimeArrivalList.first?.stationName
-//        let dummyStationName = arrivalDummyData.realtimeArrivalList.first?.stationName
-//        
-//        let requestNextId = arrayData.first?.realtimeArrivalList.first?.nextStationId
-//        let dummyNextId = arrivalDummyData.realtimeArrivalList.first?.nextStationId
-//        
-//        let requestCode = arrayData.first?.realtimeArrivalList.first?.code
-//        let dummyCode = arrivalDummyData.realtimeArrivalList.first?.code
-//        
-//        // THEN
-//        expect(requestStationName).to(
-//            equal(dummyStationName),
-//            description: "지하철 역명은 동일해야함"
-//        )
-//        
-//        expect(requestNextId).to(
-//            equal(dummyNextId),
-//            description: "기본 데이터가 같으므로, NextID도 동일해야함"
-//        )
-//        
-//        expect(requestCode).to(
-//            equal(dummyCode),
-//            description: "기본 데이터가 같으므로, code 또한 동일해야함"
-//        )
-//    }
-//    
-//    func testSingleLiveDataLoadError(){
-//        // GIVEN
-//        let data = self.arrivalErrorTotalLoadModel.singleLiveDataLoad(station: "교대")
-//        let blocking = data.toBlocking()
-//        let arrayData = try! blocking.toArray()
-//        
-//        // WHEN
-//        let requestStationName = arrayData.first?.realtimeArrivalList.first?.stationName
-//        let dummyStationName = "교대"
-//        
-//        let requestCode = arrayData.first?.realtimeArrivalList.first?.code
-//        let dummyStationCode = "현재 실시간 열차 데이터가 없어요."
-//        
-//        // THEN
-//        expect(requestStationName).to(
-//            equal(dummyStationName),
-//            description: "열차 데이터를 받아오지 못해도 역 이름은 동일해야함"
-//        )
-//        
-//        expect(requestCode).to(
-//            equal(dummyStationCode),
-//            description: "열차 데이터를 받아오지 못할 때는 (현재 실시간 열차 데이터가 없어요.)가 나와야함"
-//        )
-//    }
-//    
+    func testSingleLiveDataLoad() {
+        // GIVEN
+        let data = self.arrivalTotalLoadModel.singleLiveDataLoad(requestModel: detailArrivalDataRequestDummyModel)
+        let blocking = data.toBlocking()
+        let requestData = try! blocking.toArray().first
+        
+        let dummyData = arrivalDummyData
+        
+        // WHEN
+        let requestStationName = requestData?.first?.stationName
+        let dummyStationName = arrivalDummyData.realtimeArrivalList.first?.stationName
+        
+        let requestNextId = requestData?.first?.nextStationId
+        let dummyNextId = arrivalDummyData.realtimeArrivalList.first?.nextStationId
+        
+        let requestCode = requestData?.first?.code
+        let dummyCode = arrivalDummyData.realtimeArrivalList.first?.code
+        
+        // THEN
+        expect(requestStationName).to(
+            equal(dummyStationName),
+            description: "지하철 역명은 동일해야함"
+        )
+        
+        expect(requestNextId).to(
+            equal(dummyNextId),
+            description: "기본 데이터가 같으므로, NextID도 동일해야함"
+        )
+        
+        expect(requestCode).to(
+            equal(dummyCode),
+            description: "기본 데이터가 같으므로, code 또한 동일해야함"
+        )
+    }
+    
+    func testSingleLiveDataLoadError() {
+        // GIVEN
+        let data = self.arrivalErrorTotalLoadModel.singleLiveDataLoad(requestModel: detailArrivalDataRequestDummyModel)
+        let blocking = data.toBlocking()
+        let requestData = try! blocking.toArray().first
+        
+        let dummyData = arrivalDummyData
+        
+        // WHEN
+        let requestStationName = requestData?.first?.stationName
+        let dummyStationName = detailArrivalDataRequestDummyModel.stationName
+        
+        let requestNextId = requestData?.first?.nextStationId
+        let dummyNextId = ""
+        
+        let requestCode = requestData?.first?.code
+        let dummyCode = ""
+        
+        // THEN
+        expect(requestStationName).to(
+            equal(dummyStationName),
+            description: "열차 데이터를 받아오지 못해도 역 이름은 동일해야함"
+        )
+        
+        expect(requestNextId).to(
+            equal(dummyNextId),
+            description: "열차 데이터를 받아오지 못할 때는 nextID가 없어야함"
+        )
+        
+        expect(requestCode).to(
+            equal(dummyCode),
+            description: "열차 데이터를 받아오지 못할 때는 빈 code 값이 나와야함"
+        )
+    }
+    
     func testSeoulScheduleLoad_isFirst_isNow(){
         // GIVEN
         let data = self.seoulScheduleLoadModel.seoulScheduleLoad(scheduleGyodaeStation3Line, isFirst: true, isNow: true, isWidget: false)
