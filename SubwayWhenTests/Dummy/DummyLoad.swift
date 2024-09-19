e6ae704026b20ae82bdb9a6baf756aba27915f15
YSLee-Dev <ysleedev01@gmail.com>
2024-09-19 14:01:08 +0900

    Test(DataLoad): singleLiveDataLoad 함수 검증 로직 추가, 기존 검증 로직 제거

--- SubwayWhenTests/Dummy/DummyLoad.swift
+++ SubwayWhenTests/Dummy/DummyLoad.swift
@@ -35,8 +35,7 @@ let shinbundagSinsaStationScheduleDummyData = try! JSONDecoder().decode([Shinbun
 
 let mainCellDummyData = MainTableViewCellData(upDown: "상행", arrivalTime: "100분뒤", previousStation: "", subPrevious: "", code: "1", subWayId: "1003", stationName: "교대", lastStation: "", lineNumber: "1003", isFast: "", useLine: "", group: "", id: "-", stationCode: "340", exceptionLastStation: "", type: .real, backStationId: "1003000339", nextStationId: "1003000341", korailCode: "")
 
-
-//let detailLoadDummyData = DetailLoadData(upDown: "상행", stationName: "교대", lineNumber: "03호선", lineCode: "1003", useLine: "", stationCode: "340", exceptionLastStation: "", backStationId: "1003000339", nextStationId: "1003000341", korailCode: "")
+let detailArrivalDataRequestDummyModel = DetailArrivalDataRequestModel(upDown: "상행", stationName: "교대", lineNumber: "03호선", lineCode: "1003", exceptionLastStation: "")
 
 let url = "Test.url"
 let arrivalGyodaeStation3Line = SaveStation(id: "-", stationName: "교대", stationCode: "340", updnLine: "상행", line: "03호선", lineCode: "1003", group: .one, exceptionLastStation: "", korailCode: "")
