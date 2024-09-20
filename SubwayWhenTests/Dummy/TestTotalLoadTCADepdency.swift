96cd8c6d2aa248b8663df1aa576e54eb64d14d58
YSLee-Dev <ysleedev01@gmail.com>
2024-09-20 13:48:24 +0900

    Build(App): Dependency를 관리하는 Target 분리

---
+++ SubwayWhenTests/Dummy/TestTotalLoadTCADepdency.swift
@@ -0,0 +1,13 @@
+//
+//  TestTotalLoadTCADepdency.swift
+//  SubwayWhenTests
+//
+//  Created by 이윤수 on 9/20/24.
+//
+
+import Foundation
+import ComposableArchitecture
+
+enum TestTotalLoadTCADepdency {
+    
+}
