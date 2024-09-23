52a549a0b088cd157c681a7c08866a1442866729
YSLee-Dev <ysleedev01@gmail.com>
2024-09-23 11:22:56 +0900

    Test(Detail): 리팩토링된 DetailView를 기반으로 테스트 로직 수정 (TCA 기반)

--- SubwayWhenTests/Detail/DetailViewModelTests.swift
+++ SubwayWhenTests/Detail/DetailViewModelTests.swift
@@ -1,132 +1,122 @@
-////
-////  DetailViewModelTests.swift
-////  SubwayWhenTests
-////
-////  Created by 이윤수 on 2023/03/23.
-////
 //
-//import XCTest
+//  DetailViewModelTests.swift
+//  SubwayWhenTests
 //
-//import Nimble
-//import RxSwift
-//import RxTest
+//  Created by 이윤수 on 2023/03/23.
 //
-//@testable import SubwayWhen
-//
-//final class DetailViewModelTests: XCTestCase {
-//    var model : DetailModel!
-//    var cellData : DetailLoadData!
-//    
-//    let bag = DisposeBag()
-//    
-//    override func setUp() {
-//        let seoulSchedule = LoadModel(networkManager: NetworkManager(session: MockURLSession((response: urlResponse!, data: arrivalData))))
-//        
-//        self.model = DetailModel(totalLoadModel: TotalLoadModel(loadModel: seoulSchedule))
-//        
-//        self.cellData = .init(upDown: "상행", stationName: "교대", lineNumber: "", lineCode: "1003", useLine: "", stationCode: "", exceptionLastStation: "제외", backStationId: "", nextStationId: "", korailCode: "")
-//      
-//    }
-//
-//    func testExceptionBtnTap(){
-//        // GIVEN
-//        let testScheduler = TestScheduler(initialClock: 0)
-//        let observer = testScheduler.createObserver(DetailLoadData.self)
-//        
-//        let detailViewData = BehaviorSubject<DetailLoadData>(value: self.cellData!)
-//        
-//        detailViewData
-//            .subscribe(observer)
-//            .disposed(by: self.bag)
-//        
-//        // WHEN
-//        // 재로딩 버튼 클릭 시
-//        let exceptionBtnEvent = testScheduler.createHotObservable([
-//            .next(1, Void())
-//        ])
-//        
-//        let exceptionBtn = PublishSubject<Void>()
-//        exceptionBtnEvent
-//            .subscribe(exceptionBtn)
-//            .disposed(by: self.bag)
-//        
-//        exceptionBtn
-//            .withLatestFrom(detailViewData)
-//            .map{
-//                var now = $0
-//                now.exceptionLastStation = ""
-//                return now
-//            }
-//            .bind(to: detailViewData)
-//            .disposed(by: self.bag)
-//        
-//        testScheduler.start()
-//        
-//        // THEN
-//        expect(observer.events).to(
-//            equal([
-//                .next(0, self.cellData!),
-//                .next(1, .init(upDown: "상행", stationName: "교대", lineNumber: "", lineCode: "1003", useLine: "", stationCode: "", exceptionLastStation: "", backStationId: "", nextStationId: "", korailCode: ""))
-//            ])
-//        )
-//    }
-//    
-//    func testRealTimeData(){
-//        // GIVEN
-//        let testScheduler = TestScheduler(initialClock: 0)
-//        let detailViewData = BehaviorSubject<DetailLoadData>(value: self.cellData!)
-//        let realTime = BehaviorSubject<[Int]>(value: [])
-//        let observer = testScheduler.createObserver([Int].self)
-//        
-//        let dummy = arrivalDummyData.realtimeArrivalList
-//        
-//        realTime
-//            .subscribe(observer)
-//            .disposed(by: self.bag)
-//        
-//        // WHEN
-//        let reload = PublishSubject<Void>()
-//        let reloadEvnet = testScheduler.createHotObservable([
-//            .next(1, Void())
-//        ])
-//        reloadEvnet
-//            .subscribe(reload)
-//            .disposed(by: self.bag)
-//        
-//        let reloadData = reload
-//            .withLatestFrom(detailViewData)
-//        
-//        let reailTimeData = reloadData
-//            .withUnretained(self)
-//            .flatMapLatest{ viewModel, data in
-//                viewModel.model.arrvialDataLoad(data.stationName)
-//            }
-//        
-//        Observable.combineLatest(detailViewData, reailTimeData){[weak self] data, realTime -> [RealtimeStationArrival] in
-//            self?.model.arrivalDataMatching(station: data, arrivalData: realTime) ?? []
-//        }
-//        .map{ data in
-//            data.map{
-//                Int($0.arrivalTime) ?? 0
-//            }
-//        }
-//        .bind(to: realTime)
-//        .disposed(by: self.bag)
-//        
-//        testScheduler.start()
-//        
-//        // THEN
-//        expect(observer.events).to(
-//            equal([
-//                .next(0, []),
-//                .next(1,
-//                     [
-//                        Int(dummy[0].arrivalTime) ?? 0,
-//                        // 상하행 구분 데이터 X
-//                        Int(dummy[2].arrivalTime) ?? 1
-//                     ]
-//                     )
-//            ])
-//        )
-//    }
-//}
+
+import XCTest
+
+import ComposableArchitecture
+
+@testable import SubwayWhen
+
+final class DetailViewModelTests: XCTestCase {
+    let testDependency = TestTotalLoadTCADependency()
+
+    override func setUp()  {
+        self.testDependency.resultSchdule = seoulScheduleToResultScheduleTransformDummyData
+        self.testDependency.realtimeStationArrival = arrivalDummyData.realtimeArrivalList
+        
+        FixInfo.saveSetting.detailAutoReload = true
+        FixInfo.saveSetting.detailScheduleAutoTime = false
+        FixInfo.saveSetting.liveActivity = false
+    }
+    
+    func testDetailViewInit() async throws {
+        // GIVEN
+        let testStore = self.createStore()
+        
+        // WHEN -> View가 처음 로딩 되었을 때 상태
+        await testStore.send(.viewInitialized) // arrival, schedule 데이터 요청
+        
+        await testStore.receive(.arrivalDataRequest) {
+            // THEN
+            $0.nowArrivalLoading = true
+        }
+     
+        await testStore.receive(.scheduleDataRequest) {
+            // THEN
+            $0.nowScheduleLoading = true
+        }
+        
+        await testStore.receive(.arrivalDataRequestSuccess(self.testDependency.realtimeStationArrival)) {
+            // THEN
+            $0.nowArrivalLoading = false
+            $0.nowArrivalData = self.testDependency.realtimeStationArrival
+        }
+        
+        await testStore.receive(.timerSettingRequest) {
+            // THEN
+            $0.nowTimer = 15
+        }
+        
+        await testStore.receive(.scheduleDataRequestSuccess(self.testDependency.resultSchdule)) {
+            // THEN
+            $0.nowScheduleLoading = false
+            $0.nowScheduleData = self.testDependency.resultSchdule
+        }
+        
+        await testStore.receive(.scheduleDataSort) {
+            // THEN
+            $0.nowSculeduleSortedData = self.testDependency.resultSchdule // 시간표 sort 기능이 꺼진 상태
+        }
+    }
+    
+    func testExceptionLastStationBtnTapped() async throws {
+        // GIVEN
+        var model = detailSendModelDummyData
+        model.exceptionLastStation = "구파발"
+        let testStore = self.createStore(sendedLoadModel: model)
+        
+        // WHEN
+        await testStore.send(.viewInitialized)
+        try await Task.sleep(for: .milliseconds(350)) // 값 세팅 대기
+        
+        await testStore.send(.exceptionLastStationBtnTapped) {
+            // THEN
+            XCTAssertNotNil($0.dialogState)
+        }
+        
+        // WHEN -> OK 버튼 클릭
+        await testStore.send(.dialogAction(.presented(.okBtnTapped))) {
+            // THEN
+            $0.dialogState = nil
+            $0.sendedLoadModel.exceptionLastStation = ""
+        }
+        
+        // THEN -> Arrival, schedule 순서
+        await testStore.receive(.arrivalDataRequest)
+        await testStore.receive(.scheduleDataRequest)
+    }
+    
+    func testRefreshBtnTapped() async throws {
+        // GIVEN
+        let testStore = self.createStore()
+        
+        // WHEN
+        await testStore.send(.viewInitialized)
+        try await Task.sleep(for: .milliseconds(350)) // 값 세팅 대기
+        
+        await testStore.send(.refreshBtnTapped) {
+            // THEN
+            $0.nowTimer = nil
+        }
+        
+        // THEN -> Arrival, schedule 순서
+        await testStore.receive(.arrivalDataRequest)
+        await testStore.receive(.scheduleDataSort)
+    }
+}
+
+private extension DetailViewModelTests {
+    func createStore(isDisposable: Bool = false, sendedLoadModel: DetailSendModel = detailSendModelDummyData, exhaustivity: Bool = false) -> TestStore<DetailFeature.State,  DetailFeature.Action> {
+        let store =  TestStore(initialState: DetailFeature.State(isDisposable: isDisposable, sendedLoadModel: sendedLoadModel)) {
+            DetailFeature()
+        } withDependencies: { dependency in
+            dependency.totalLoad = self.testDependency
+        }
+        store.exhaustivity = exhaustivity ? .on : .off(showSkippedAssertions: false)
+        return store
+    }
+}
