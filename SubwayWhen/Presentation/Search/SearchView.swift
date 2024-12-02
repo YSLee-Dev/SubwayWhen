79dd7b2a61463fc620c4296dc30f8a8c3cec7a27
YSLee-Dev <ysleedev01@gmail.com>
2024-12-02 16:45:05 +0900

    Feat(Search): SearchView 추가 (SearchVC를 대체하는 SwiftUI View)

---
+++ SubwayWhen/Presentation/Search/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 12/2/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        NavigationBarScrollViewInSUI(title: "검색") {
+            Text("")
+        }
+    }
+}
+
+#Preview {
+    SearchView()
+}
