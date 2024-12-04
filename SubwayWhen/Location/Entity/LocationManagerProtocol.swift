5764d0816cd656354c5a24b6e1fbef2e7f467243
YSLee-Dev <ysleedev01@gmail.com>
2024-12-04 13:32:10 +0900

    Feat(Location): 위치 권한, 위치 정보를 관리하는 매니저 파일 제작

---
+++ SubwayWhen/Location/Entity/LocationManagerProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  LocationManagerProtocol.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 12/4/24.
+//
+
+import Foundation
+
+protocol LocationManagerProtocol {
+    
+}
---
+++ SubwayWhen/Location/LocationManager.swift
@@ -0,0 +1,12 @@
+//
+//  LocationManager.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 12/4/24.
+//
+
+import Foundation
+
+class LocationManager: LocationManagerProtocol {
+    
+}
