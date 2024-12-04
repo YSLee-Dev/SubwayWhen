61f9d9c37c0a537647d214daddac0a00446569d1
YSLee-Dev <ysleedev01@gmail.com>
2024-12-04 10:56:42 +0900

    Feat(Search): Feature 파일 추가

---
+++ SubwayWhen/Presentation/Search/SearchFeature.swift
@@ -0,0 +1,30 @@
+//
+//  SearchFeature.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 12/4/24.
+//
+
+import Foundation
+
+import ComposableArchitecture
+import FirebaseAnalytics
+
+@Reducer
+class SearchFeature {
+    
+    @ObservableState
+    struct State: Equatable {
+        
+    }
+    
+    enum Action: Equatable {
+        
+    }
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            return .none
+        }
+    }
+}
