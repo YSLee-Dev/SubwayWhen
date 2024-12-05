213912f82c534e76b88592260078781c2d9d8087
YSLee-Dev <ysleedev01@gmail.com>
2024-12-06 08:56:36 +0900

    Feat(Search): 추천역을 보여주는 View 추가 및 SearchView 적용

---
+++ SubwayWhen/Presentation/Search/Sub/SearchWordRecommendView.swift
@@ -0,0 +1,50 @@
+//
+//  SearchWordRecommendView.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 12/5/24.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct SearchWordRecommendView: View {
+    @Binding var store: StoreOf<SearchFeature>
+    
+    var body: some View {
+        MainStyleViewInSUI {
+            VStack(spacing: 15) {
+                HStack {
+                    Text("자주 검색되는 지하철역")
+                        .font(.system(size: ViewStyle.FontSize.mediumSize, weight: .heavy))
+                    Spacer()
+                }
+            
+                LazyVGrid(columns: [GridItem(.flexible(), spacing: 15), GridItem(.flexible(), spacing: 15)]) {
+                    ForEach(self.store.state.recommendStationList, id: \.self) { data in
+                        HStack {
+                            Spacer()
+                            Text(data)
+                                .foregroundStyle(.black.opacity(0.8))
+                                .font(.system(size: ViewStyle.FontSize.smallSize, weight: .semibold))
+                            Spacer()
+                        }
+                        .padding(15)
+                        .background {
+                            RoundedRectangle(cornerRadius: ViewStyle.Layer.radius)
+                                .fill(Color.gray.opacity(0.2))
+                        }
+                        .padding(.vertical, 5)
+                    }
+                }
+            }
+            .padding(.init(top: 20, leading: 15, bottom: 20, trailing: 15))
+        }
+        .padding(.bottom, 20)
+    }
+}
+
+
+#Preview {
+    SearchWordRecommendView(store: .constant(.init(initialState: .init(), reducer: {SearchFeature()})))
+}
