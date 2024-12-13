9c952223532918e1764d8c02fb55d835e1042180
YSLee-Dev <ysleedev01@gmail.com>
2024-12-13 14:54:01 +0900

    Refactor(TotalLoad): 지하철 노선을 하나의 struct으로 관리하도록 리팩토링

--- SubwayWhen/Presentation/Search/Sub/SearchStationResultView.swift
+++ SubwayWhen/Presentation/Search/Sub/SearchStationResultView.swift
@@ -37,7 +37,7 @@ struct SearchStationResultView: View {
                         ForEach(Array(zip(self.store.nowStationSearchList, self.store.nowStationSearchList.indices)), id: \.1) { data, index in
                             AnimationButtonInSUI(bgColor: Color.gray.opacity(0.1), tappedBGColor: Color.gray.opacity(0.01), buttonView: {
                                 HStack(spacing: 10) {
-                                    StationTitleViewInSUI(title: data.useLine, lineColor: data.lineNumber.rawValue, size: 60, isFill: true)
+                                    StationTitleViewInSUI(title: data.line.useLine, lineColor: data.line.rawValue, size: 60, isFill: true)
                                     
                                     Text(data.stationName)
                                         .font(.system(size: ViewStyle.FontSize.mediumSize, weight: .bold))
