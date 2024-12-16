8878e14f98e815d083f551390f4505d921ce1940
YSLee-Dev <ysleedev01@gmail.com>
2024-12-16 13:51:49 +0900

    Fix(Search): 애니메이션이 중복되던 오류 수정

--- SubwayWhen/Presentation/Search/SearchFeature.swift
+++ SubwayWhen/Presentation/Search/SearchFeature.swift
@@ -305,7 +305,7 @@ class SearchFeature: NSObject {
                 return .concatenate([
                     .cancel(id: Key.searchDelay),
                     .run { send in
-                        try await Task.sleep(for: .milliseconds(800))
+                        try await Task.sleep(for: .milliseconds(700))
                         await send(.stationSearchRequest)
                     }
                 ])
@@ -317,7 +317,7 @@ class SearchFeature: NSObject {
                 ])
                 return .run { [name = state.searchQuery] send in
                     let result = await self.totalLoad.stationNameSearchReponse(name)
-                    try await Task.sleep(for: .milliseconds(200))
+                    try await Task.sleep(for: .milliseconds(350))
                     await send(.stationSearchResult(result))
                 }
                 
