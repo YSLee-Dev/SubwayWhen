2d38b3f1515d32370811c0318a3dc66bd2cf87ba
YSLee-Dev <ysleedev01@gmail.com>
2026-01-06 17:49:14 +0900

    Feat(Main): CongestionModalVC 파일 추가

---
+++ SubwayWhen/Presentation/Main/CongestionModal/CongestionModalVC.swift
@@ -0,0 +1,16 @@
+//
+//  CongestionModalVC.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 1/6/26.
+//
+
+import UIKit
+
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class CongestionModalVC : ModalVCCustom {
+    
+}
