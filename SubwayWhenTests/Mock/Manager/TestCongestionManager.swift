118a23cbafb907befb734a269aeef56dc7186050
YSLee-Dev <ysleedev01@gmail.com>
2026-01-15 15:25:56 +0900

    Test(Mock): 실제 Manager가 수정됨에 따라 TestManager도 수정

--- SubwayWhenTests/Mock/Manager/TestCongestionManager.swift
+++ SubwayWhenTests/Mock/Manager/TestCongestionManager.swift
@@ -47,6 +47,10 @@ final class TestCongestionManager: CongestionManagerProtocol {
         default: dayData = stationData.weekday
         }
         
+        if dayData.isEmpty {
+            return dayData
+        }
+        
         for hour in 1...4 {
             if dayData["\(hour)"] == nil {
                 dayData["\(hour)"] = CongestionLevel(percent: 0, level: 0)
