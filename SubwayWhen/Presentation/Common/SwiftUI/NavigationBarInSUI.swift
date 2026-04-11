38342f511c194d9f757332fcb2748ff2d8bc40c8
YSLee-Dev <ysleedev01@gmail.com>
2026-04-11 13:30:04 +0900

    Refactor(App): NavigationBarScrollViewInSUI에서 NavigationBarInSUI 분리 (CC)

---
+++ SubwayWhen/Presentation/Common/SwiftUI/NavigationBarInSUI.swift
@@ -0,0 +1,56 @@
+//
+//  NavigationBarInSUI.swift
+//  SubwayWhen
+//
+//  Created by 이윤수 on 4/11/26
+//
+
+import SwiftUI
+
+struct NavigationBarInSUI: View {
+
+    // MARK: - Properties
+
+    private let title: String
+    private let backBtnTapped: (() -> ())?
+    private let backBtnIcon: String?
+    @Binding private var isSubTitleShow: Bool
+
+    init(title: String, isSubTitleShow: Binding<Bool>, backBtnTapped: (() -> ())? = nil, backBtnIcon: String? = nil) {
+        self.title = title
+        self._isSubTitleShow = isSubTitleShow
+        self.backBtnTapped = backBtnTapped
+        self.backBtnIcon = backBtnIcon
+    }
+
+    // MARK: - View
+
+    var body: some View {
+        HStack {
+            if self.backBtnTapped != nil {
+                Button(action: {
+                    self.backBtnTapped!()
+                }) {
+                    Image(systemName: "\(self.backBtnIcon ?? "")")
+                        .foregroundColor(.init(uiColor: .label))
+                }
+            }
+
+            Text(self.title)
+                .font(.system(size: ViewStyle.FontSize.largeSize, weight: .bold))
+                .padding(.leading, 1)
+                .opacity(self.isSubTitleShow ? 1 : 0)
+                .offset(y: self.isSubTitleShow ? 0 : 7.5)
+                .animation(.smooth(duration: 0.25), value: self.isSubTitleShow)
+
+            Spacer()
+        }
+        .frame(height: 45)
+        .padding(.horizontal, ViewStyle.padding.mainStyleViewLR)
+    }
+}
+
+#Preview {
+    @Previewable @State var isShow = false
+    NavigationBarInSUI(title: "상세화면", isSubTitleShow: $isShow, backBtnTapped: {})
+}
