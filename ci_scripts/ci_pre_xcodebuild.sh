6563ac324848c6a27ec813f5d2a510e3c61cbb86
YSLee-Dev <ysleedev01@gmail.com>
2026-04-29 21:02:05 +0900

    Build(App): Xcode Cloud RequestToken.plist 환경 변수 주입 스크립트 추가 (CC)

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidation -bool YES
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+
+cat > "$CI_PRIMARY_REPOSITORY_PATH/SubwayWhen/RequestToken.plist" << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>REALTIME_TOKEN</key>
+    <string>$REALTIME_TOKEN</string>
+    <key>LIVE_TOKEN</key>
+    <string>$LIVE_TOKEN</string>
+    <key>KORAIL_TOKEN</key>
+    <string>$KORAIL_TOKEN</string>
+    <key>SEOUL_TOKEN</key>
+    <string>$SEOUL_TOKEN</string>
+    <key>KAKAO_TOKEN</key>
+    <string>$KAKAO_TOKEN</string>
+</dict>
+</plist>
+EOF
