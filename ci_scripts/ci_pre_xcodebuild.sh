e0b4baf2f139c666e2e961dd24823c189919117e
YSLee-Dev <ysleedev01@gmail.com>
2026-05-03 13:27:46 +0900

    Build(App): RequestToken.plist 생성 방식 PlistBuddy로 변경 (CC)

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -7,21 +7,9 @@ REPO_PATH="${CI_PRIMARY_REPOSITORY_PATH:-/Volumes/workspace/repository}"
 echo "$GOOGLE_SERVICE_INFO_PLIST_MAIN" | base64 --decode > "$REPO_PATH/SubwayWhen/Application/GoogleService-Info.plist"
 echo "$GOOGLE_SERVICE_INFO_PLIST_MAIN" | base64 --decode > "$REPO_PATH/SubwayWhenHomeWidget/GoogleService-Info.plist"
 
-cat > "$REPO_PATH/SubwayWhen/RequestToken.plist" << EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-    <key>REALTIME_TOKEN</key>
-    <string>$REALTIME_TOKEN</string>
-    <key>LIVE_TOKEN</key>
-    <string>$LIVE_TOKEN</string>
-    <key>KORAIL_TOKEN</key>
-    <string>$KORAIL_TOKEN</string>
-    <key>SEOUL_TOKEN</key>
-    <string>$SEOUL_TOKEN</string>
-    <key>KAKAO_TOKEN</key>
-    <string>$KAKAO_TOKEN</string>
-</dict>
-</plist>
-EOF
+PLIST="$REPO_PATH/SubwayWhen/RequestToken.plist"
+/usr/libexec/PlistBuddy -c "Add :REALTIME_TOKEN string ${REALTIME_TOKEN}" "$PLIST"
+/usr/libexec/PlistBuddy -c "Add :LIVE_TOKEN string ${LIVE_TOKEN}" "$PLIST"
+/usr/libexec/PlistBuddy -c "Add :KORAIL_TOKEN string ${KORAIL_TOKEN}" "$PLIST"
+/usr/libexec/PlistBuddy -c "Add :SEOUL_TOKEN string ${SEOUL_TOKEN}" "$PLIST"
+/usr/libexec/PlistBuddy -c "Add :KAKAO_TOKEN string ${KAKAO_TOKEN}" "$PLIST"
