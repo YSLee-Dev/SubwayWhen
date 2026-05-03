e1625f73c2c669f98656fbe9caf7f69857ff6cb0
YSLee-Dev <ysleedev01@gmail.com>
2026-05-03 14:02:59 +0900

    Build(App): ci_pre_xcodebuild.sh plist 생성 전면 Python 전환 (CC)

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,28 +1,49 @@
 #!/bin/sh
+set -e
+
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidation -bool YES
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 
-REPO_PATH="${CI_PRIMARY_REPOSITORY_PATH:-/Volumes/workspace/repository}"
-
-echo "$GOOGLE_SERVICE_INFO_PLIST_MAIN" | base64 --decode > "$REPO_PATH/SubwayWhen/Application/GoogleService-Info.plist"
-echo "$GOOGLE_SERVICE_INFO_PLIST_MAIN" | base64 --decode > "$REPO_PATH/SubwayWhenHomeWidget/GoogleService-Info.plist"
-
 python3 << 'PYEOF'
-import plistlib, os
+import base64, os, plistlib, sys
 
 repo_path = os.environ.get('CI_PRIMARY_REPOSITORY_PATH') or '/Volumes/workspace/repository'
-plist_path = repo_path + '/SubwayWhen/RequestToken.plist'
+print('[CI] Repository path:', repo_path)
 
-data = {
-    'REALTIME_TOKEN': os.environ.get('REALTIME_TOKEN', ''),
-    'LIVE_TOKEN':     os.environ.get('LIVE_TOKEN', ''),
-    'KORAIL_TOKEN':   os.environ.get('KORAIL_TOKEN', ''),
-    'SEOUL_TOKEN':    os.environ.get('SEOUL_TOKEN', ''),
-    'KAKAO_TOKEN':    os.environ.get('KAKAO_TOKEN', ''),
-}
+# GoogleService-Info.plist
+b64 = os.environ.get('GOOGLE_SERVICE_INFO_PLIST_MAIN', '').strip()
+if not b64:
+    print('[CI] ERROR: GOOGLE_SERVICE_INFO_PLIST_MAIN is not set', file=sys.stderr)
+    sys.exit(1)
 
-with open(plist_path, 'wb') as f:
-    plistlib.dump(data, f, fmt=plistlib.FMT_XML)
+google_plist_data = base64.decodebytes(b64.encode())
 
-print('RequestToken.plist created at:', plist_path)
+try:
+    plistlib.loads(google_plist_data)
+except Exception as e:
+    print('[CI] ERROR: decoded GoogleService-Info.plist is not a valid plist:', e, file=sys.stderr)
+    sys.exit(1)
+
+for dest in [
+    repo_path + '/SubwayWhen/Application/GoogleService-Info.plist',
+    repo_path + '/SubwayWhenHomeWidget/GoogleService-Info.plist',
+]:
+    with open(dest, 'wb') as f:
+        f.write(google_plist_data)
+    print('[CI] Created:', dest)
+
+# RequestToken.plist
+token_keys = ['REALTIME_TOKEN', 'LIVE_TOKEN', 'KORAIL_TOKEN', 'SEOUL_TOKEN', 'KAKAO_TOKEN']
+for key in token_keys:
+    if not os.environ.get(key):
+        print('[CI] WARNING:', key, 'is not set', file=sys.stderr)
+
+token_path = repo_path + '/SubwayWhen/RequestToken.plist'
+with open(token_path, 'wb') as f:
+    plistlib.dump(
+        {k: os.environ.get(k, '') for k in token_keys},
+        f,
+        fmt=plistlib.FMT_XML,
+    )
+print('[CI] Created:', token_path)
 PYEOF
